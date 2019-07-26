93ded3de5e5a80ea5ad29011a33794d10f5c0f58
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-07-26 17:48:18 +0100

    add Pac4jConfig test

---
+++ app/src/test/kotlin/org/imperial/mrc/modelserver/unit/security/Pac4jConfigTests.kt
@@ -0,0 +1,22 @@
+package org.imperial.mrc.modelserver.unit.security
+
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions
+import org.imperial.mrc.modelserver.security.Pac4jConfig
+import org.pac4j.http.client.indirect.FormClient
+
+class Pac4jConfigTests
+{
+    @Test
+    fun `can get config`()
+    {
+        val sut = Pac4jConfig()
+        val config = sut.config()
+
+        Assertions.assertThat(config.clients.callbackUrl).isEqualTo("/callback")
+        Assertions.assertThat(config.clients.clients.count()).isEqualTo(1)
+        val client = config.clients.clients.first()
+        Assertions.assertThat(client).isInstanceOf(FormClient::class.java)
+        Assertions.assertThat((client as FormClient).loginUrl).isEqualTo("/login")
+    }
+}
