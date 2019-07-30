0d4eba3e2869173a2bfe3d513bc4cf08f715406d
Alex <alex.hill@gmail.com>
2019-07-30 11:58:08 +0100

    use network and remove unused deps

--- scripts/run-dependencies.sh
+++ scripts/run-dependencies.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
-docker run --rm -d --network=host --name modelapi_redis redis
-docker run --rm -d --network=host --name modelapi_model mrcide/modelapi:latest
+set -ex
 
-docker run --rm -d --network=host --name hint-db mrcide/hint-db:mrc-371
+NETWORK=hint_nw
+docker network create $NETWORK
+
+docker run --rm -d \
+  --network=$NETWORK \
+  --name hint-db \
+  mrcide/hint-db:mrc-371
+
+# From now on, if the user presses Ctrl+C we should teardown gracefully
+trap on_interrupt INT
+function on_interrupt() {
+  docker stop hint-db
+  docker rm hint-db
+  docker network rm $NETWORK
+}
 
 # Need to give the database a little time to initialise before we can run the migration
 sleep 10s
-docker run --rm --network=host --name hint-db-migrate mrcide/hint-db-migrate:mrc-371 -url=jdbc:postgresql://localhost/hint
+docker run --rm --network=$NETWORK mrcide/hint-db-migrate:mrc-371
+
+# Wait for Ctrl+C
+echo "Ready to use. Press Ctrl+C to teardown."
+sleep infinity
