2b9b492d1e499f045ffda03cc7d6c8b33b19a63f
Alex <alex.hill@gmail.com>
2019-07-31 15:27:01 +0100

    backend for pjnz upload

--- app/src/main/kotlin/org/imperial/mrc/hint/HintApplication.kt
+++ app/src/main/kotlin/org/imperial/mrc/hint/HintApplication.kt
@@ -2,6 +2,7 @@ package org.imperial.mrc.hint
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 class HintApplication
--- app/src/main/kotlin/org/imperial/mrc/hint/security/SecurityConfig.kt
+++ app/src/main/kotlin/org/imperial/mrc/hint/security/SecurityConfig.kt
@@ -2,15 +2,12 @@ package org.imperial.mrc.hint.security
 
 import org.pac4j.core.config.Config
 import org.pac4j.springframework.web.SecurityInterceptor
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@ComponentScan(basePackages = ["org.pac4j.springframework.web"])
-class SecurityConfig(@Autowired val config: Config): WebMvcConfigurerAdapter() {
+class SecurityConfig(val config: Config): WebMvcConfigurer {
 
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(SecurityInterceptor(config, "FormClient"))
