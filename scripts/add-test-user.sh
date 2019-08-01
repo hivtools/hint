ff31b380f2bc261d13351d35849e99c61ff22d88
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-08-01 10:20:05 +0100

    Setting bionic did not work, try without os

--- scripts/add-test-user.sh
+++ scripts/add-test-user.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 NETWORK=hint_nw
 
-image=mrcide/hint-user-cli:mrc-373 #TODO: change this to latest after merge
+image=mrcide/hint-user-cli:mrc-373 #TODO: change this to master after merge
 docker run --network=hint_nw $image add-user test.user@example.com password
