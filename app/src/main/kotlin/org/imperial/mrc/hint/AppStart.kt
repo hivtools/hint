64326345039658a0d811e686d97de1f1b05ea31c
Alex <alex.hill@gmail.com>
2019-08-01 10:06:47 +0100

    revert unneeded change

--- app/src/main/kotlin/org/imperial/mrc/hint/AppStart.kt
+++ app/src/main/kotlin/org/imperial/mrc/hint/AppStart.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class Hint
+class HintApplication
 
 fun main(args: Array<String>) {
-    SpringApplication.run(Hint::class.java, *args)
+    SpringApplication.run(HintApplication::class.java, *args)
 }
