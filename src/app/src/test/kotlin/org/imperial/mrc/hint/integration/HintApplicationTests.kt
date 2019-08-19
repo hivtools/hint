19171ce809a71fde1ca871f2e7b0044dfa9e6e9b
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-08-19 15:00:21 +0100

    Added login test

--- src/app/src/test/kotlin/org/imperial/mrc/hint/integration/HintApplicationTests.kt
+++ src/app/src/test/kotlin/org/imperial/mrc/hint/integration/HintApplicationTests.kt
@@ -5,9 +5,15 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.exchange
 import org.springframework.boot.test.web.client.getForEntity
-import org.springframework.http.HttpStatus
+import org.springframework.boot.test.web.client.postForEntity
+import org.springframework.http.*
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.util.LinkedMultiValueMap
 
+
+@ActiveProfiles(profiles=["test"])
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HintApplicationTests(@Autowired val restTemplate: TestRestTemplate) {
 
@@ -20,6 +26,50 @@ class HintApplicationTests(@Autowired val restTemplate: TestRestTemplate) {
         val entity = restTemplate.getForEntity<String>("/")
         assertThat(entity.body!!).contains("Log In")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+    }
+
+    @Test
+    fun `can login with correct credentials`() {
+        val map = LinkedMultiValueMap<String, String>()
+        map.add("username", "test.user@example.com")
+        map.add("password", "password")
+
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_FORM_URLENCODED
+
+        val entity = restTemplate.postForEntity<String>("/callback/", HttpEntity(map, headers))
+
+        //test get redirected to '/'
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.FOUND)
+        assertThat(entity.headers["Location"]!!.first()).isEqualTo("/")
+
+        val cookies = entity.headers["Set-Cookie"]!!.first().split(";")
+        val sessionCookie = cookies.first{it.startsWith("JSESSIONID")}.split("=")[1]
+
+        //Assert can access '/' using the cookie
+        val rootHeaders = HttpHeaders()
+        rootHeaders.put("Cookie", listOf("JSESSIONID=" + sessionCookie))
+        val getRootEntity = HttpEntity(null, rootHeaders)
+        val rootEntity = restTemplate.exchange("/", HttpMethod.GET, getRootEntity, String::class.java)
+
+        assertThat(rootEntity.statusCode).isEqualTo(HttpStatus.OK)
+    }
+
+    @Test
+    fun `redirect back to login page on login with incorrect correct credentials`() {
+        val map = LinkedMultiValueMap<String, String>()
+        map.add("username", "test.user@example.com")
+        map.add("password", "badpassword")
+
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_FORM_URLENCODED
+
+        val entity = restTemplate.postForEntity<String>("/callback/", HttpEntity(map, headers))
+
+        //test get redirected back to login page
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.FOUND)
+        assertThat(entity.headers["Location"]!!.first())
+                .isEqualTo("/login?username=test.user%40example.com&error=BadCredentialsException")
     }
 
 }
