72b678752b822980e36f2fdebb99d1c85c800d21
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-08-19 13:52:04 +0100

    Updated security config test

--- src/app/src/test/kotlin/org/imperial/mrc/hint/unit/security/Pac4jConfigTests.kt
+++ src/app/src/test/kotlin/org/imperial/mrc/hint/unit/security/Pac4jConfigTests.kt
@@ -3,14 +3,24 @@ package org.imperial.mrc.hint.unit.security
 import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions
 import org.imperial.mrc.hint.security.Pac4jConfig
+import org.junit.jupiter.api.extension.ExtendWith
+import org.pac4j.core.client.BaseClient
 import org.pac4j.http.client.indirect.FormClient
+import org.pac4j.sql.profile.service.DbProfileService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
+@SpringBootTest
+@ExtendWith(SpringExtension::class)
 class Pac4jConfigTests
 {
+    @Autowired
+    private lateinit var sut: Pac4jConfig
+
     @Test
     fun `can get config`()
     {
-        val sut = Pac4jConfig()
         val config = sut.getConfig()
 
         Assertions.assertThat(config.clients.callbackUrl).isEqualTo("/callback")
@@ -18,5 +28,9 @@ class Pac4jConfigTests
         val client = config.clients.clients.first()
         Assertions.assertThat(client).isInstanceOf(FormClient::class.java)
         Assertions.assertThat((client as FormClient).loginUrl).isEqualTo("/login")
+
+        val field = BaseClient::class.java.getDeclaredField("authenticator")
+        field.isAccessible = true
+        Assertions.assertThat(field.get(client)).isInstanceOf(DbProfileService::class.java)
     }
 }
