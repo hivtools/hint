3c117368da54b03dc251f502bd62866ca70306b8
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-08-20 17:46:44 +0100

    Added actions tests

---
+++ src/app/src/test/kotlin/org/imperial/mrc/hint/unit/templates/ForgotPasswordTests.kt
@@ -0,0 +1,20 @@
+package org.imperial.mrc.hint.unit.templates
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.ui.ConcurrentModel
+
+class ForgotPasswordTests {
+    companion object {
+        val template = FreemarkerTemplateLoader("forgot-password.ftl")
+
+    }
+
+    @Test
+    fun `renders as expected`() {
+        val doc = ForgotPasswordTests.template.jsoupDocFor(ConcurrentModel())
+
+        Assertions.assertThat(doc.select("title").text()).isEqualTo("Forgot password")
+        Assertions.assertThat(doc.select("#app-forgot-password").count()).isEqualTo(1)
+    }
+}
---
+++ src/app/static/src/tests/components/password/forgotPassword.test.ts
@@ -0,0 +1,11 @@
+describe("Forgot password component", () => {
+    //renders form with no error
+
+    //renders with error
+
+    //renders with link requested message
+
+    //invokes requestLink action
+
+    //does not requestLink action if form is invalid - empty, not email address
+});
