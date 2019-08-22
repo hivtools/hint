a01ffa1d3a951a1775ef1951076162015bced92a
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-08-22 12:01:04 +0100

    Added AppProperties test

--- src/app/src/test/kotlin/org/imperial/mrc/hint/unit/AppPropertiesTest.kt
+++ src/app/src/test/kotlin/org/imperial/mrc/hint/unit/AppPropertiesTest.kt
@@ -1,6 +1,7 @@
 package org.imperial.mrc.hint.unit
 
 import org.assertj.core.api.Java6Assertions.assertThat
+import org.imperial.mrc.hint.AppProperties
 import org.imperial.mrc.hint.ConfiguredAppProperties
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
@@ -13,12 +14,16 @@ class AppPropertiesTests {
         File("tmp").deleteRecursively()
     }
 
-    @Test
-    fun `properties can be read from disk`() {
-
+    private fun makeTempConfigFile(): File {
         File("tmp").mkdir()
         val config = File("tmp/fake.properties")
         config.createNewFile()
+        return config
+    }
+
+    @Test
+    fun `properties can be read from disk`() {
+        val config = makeTempConfigFile()
         config.writeText("something=1234")
         val props = ConfiguredAppProperties.readProperties("tmp/fake.properties")
         assertThat(props["something"]).isEqualTo("1234")
@@ -27,5 +32,21 @@ class AppPropertiesTests {
     @Test
     fun `properties are expected at the correct path`() {
         assertThat(ConfiguredAppProperties.configPath).isEqualTo("/etc/hint/config.properties")
+    }
+
+    @Test
+    fun `can read uploadDirectory`{
+        val config = makeTempConfigFile()
+        config.writeText("upload_dir=/fakedir")
+
+        assertThat((ConfiguredAppProperties as AppProperties).uploadDirectory).isEqualTo("/fakedir")
+    }
+
+    @Test
+    fun `can read tokenIssuer`{
+        val config = makeTempConfigFile()
+        config.writeText("token_issuer=fakeIssuer")
+
+        assertThat((ConfiguredAppProperties as AppProperties).tokenIssuer).isEqualTo("fakeIssuer")
     }
 }
