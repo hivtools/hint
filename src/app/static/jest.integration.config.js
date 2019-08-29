e2e13530b59ca7862bbccb03ac4de5d60466f69a
Alex <alex.hill@gmail.com>
2019-08-29 18:38:02 +0100

    collect coverage for unit tests and integration tests

--- src/app/static/jest.integration.config.js
+++ src/app/static/jest.integration.config.js
@@ -2,4 +2,5 @@ var config = require('./jest.config');
 
 config.testRegex = "(/__tests__/.*|(\\.|/)(itest))\\.[jt]sx?$";
 config.globals = {...config.globals, "appUrl": "http://localhost:8080/"};
+config.overageDirectory = "./coverage/integration/";
 module.exports = config;
