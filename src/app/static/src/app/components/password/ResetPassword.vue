91e184c3fec280f35ed002a665669c2a4d342c64
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-08-29 17:33:37 +0100

    Revert test change

--- src/app/static/src/app/components/password/ResetPassword.vue
+++ src/app/static/src/app/components/password/ResetPassword.vue
@@ -6,7 +6,7 @@
                 <form ref="resetPasswordForm" class="needs-validation" novalidate>
                     <div class="form-group">
                         <input type="password" class="form-control" name="password" id="email" placeholder="New password"
-                               v-model="password" pattern=".{2,}" required>
+                               v-model="password" pattern=".{6,}" required>
                         <div class="invalid-feedback">Please enter a new password with at least 6 characters.</div>
                     </div>
 
