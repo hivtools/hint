eeea118541509c11a059a59f1af3b68ee84d0cb9
Alex <alex.hill@gmail.com>
2019-08-30 17:17:24 +0100

    add dev script for local integration testing

---
+++ src/app/static/scripts/run-dev-dependencies-for-integration-tests.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -ex
+
+HERE=$(readlink -f "$(dirname $0)")
+NETWORK=hint_nw
+HINT=hint
+DB=hint_db
+API=hintr
+$HERE/../../../../scripts/run-dependencies.sh
+HINT_VERSION=mrc-433_integrationtests
+TEST_CONFIG=$HERE/test.properties
+HINT_IMAGE=mrcide/$HINT:$HINT_VERSION
+
+docker run --rm -d \
+  --network=$NETWORK \
+  --name $HINT \
+  -p 8080:8080 \
+  -v $TEST_CONFIG:/etc/hint/config.properties \
+  $HINT_IMAGE
+
+# From now on, if the user presses Ctrl+C we should teardown gracefully
+trap cleanup INT
+trap cleanup ERR
+function cleanup() {
+  docker kill $DB
+  docker kill $API
+  docker kill $HINT
+  docker network rm $NETWORK
+}
+
+# Wait for Ctrl+C
+echo "Ready to use. Press Ctrl+C to teardown."
+sleep infinity
