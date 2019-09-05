77f69f703b0c0da7e29c256830d1c506a2a91e9c
Alex <alex.hill@gmail.com>
2019-09-05 10:31:52 +0100

    check types

--- src/app/src/test/kotlin/org/imperial/mrc/hint/integration/DiseaseTests.kt
+++ src/app/src/test/kotlin/org/imperial/mrc/hint/integration/DiseaseTests.kt
@@ -1,10 +1,10 @@
 package org.imperial.mrc.hint.integration
 
+import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.imperial.mrc.hint.helpers.getTestEntity
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
 import org.springframework.boot.test.web.client.postForEntity
-import org.springframework.http.HttpStatus
 
 class DiseaseTests: SecureIntegrationTests() {
 
@@ -14,6 +14,10 @@ class DiseaseTests: SecureIntegrationTests() {
         val postEntity = getTestEntity("survey.csv")
         val entity = testRestTemplate.postForEntity<String>("/disease/survey/", postEntity)
         assertSecureWithSuccess(isAuthorized, entity, "ValidateInputResponse")
+        if (isAuthorized == IsAuthorized.TRUE){
+            val data = getResponseData(entity)
+            assertThat(data["type"].asText()).isEqualTo("survey")
+        }
     }
 
     @ParameterizedTest
@@ -22,6 +26,11 @@ class DiseaseTests: SecureIntegrationTests() {
         val postEntity = getTestEntity("programme.csv")
         val entity = testRestTemplate.postForEntity<String>("/disease/program/", postEntity)
         assertSecureWithSuccess(isAuthorized, entity, "ValidateInputResponse")
+
+        if (isAuthorized == IsAuthorized.TRUE){
+            val data = getResponseData(entity)
+            assertThat(data["type"].asText()).isEqualTo("programme")
+        }
     }
 
     @ParameterizedTest
@@ -30,5 +39,10 @@ class DiseaseTests: SecureIntegrationTests() {
         val postEntity = getTestEntity("anc.csv")
         val entity = testRestTemplate.postForEntity<String>("/disease/anc/", postEntity)
         assertSecureWithSuccess(isAuthorized, entity, "ValidateInputResponse")
+
+        if (isAuthorized == IsAuthorized.TRUE){
+            val data = getResponseData(entity)
+            assertThat(data["type"].asText()).isEqualTo("anc")
+        }
     }
 }
