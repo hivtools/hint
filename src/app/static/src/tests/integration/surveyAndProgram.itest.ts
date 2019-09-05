790dd3e15402b984d657c12fbef7d2845f758f5a
Alex <alex.hill@gmail.com>
2019-09-05 10:50:53 +0100

    use real files in front end integration tests

--- src/app/static/src/tests/integration/surveyAndProgram.itest.ts
+++ src/app/static/src/tests/integration/surveyAndProgram.itest.ts
@@ -5,60 +5,40 @@ const FormData = require("form-data");
 
 describe("Survey and program actions", () => {
 
-    beforeEach(() => {
-        fs.writeFileSync("fakefile");
-    });
-
-    afterEach(() => {
-        fs.unlinkSync("fakefile")
-    });
-
     it("can upload survey", async () => {
 
         const commit = jest.fn();
 
-        const file = fs.createReadStream("fakefile");
+        const file = fs.createReadStream("../testdata/survey.csv");
         const formData = new FormData();
         formData.append('file', file);
 
         await actions.uploadSurvey({commit} as any, formData);
-
-        expect(commit.mock.calls[0][0]).toStrictEqual({
-            type: "SurveyError",
-            payload: "could not find function \"validate_func\""
-        });
+        expect(commit.mock.calls[0][0]["type"]).toBe("SurveyLoaded");
     });
 
     it("can upload program", async () => {
 
         const commit = jest.fn();
 
-        const file = fs.createReadStream("fakefile");
+        const file = fs.createReadStream("../testdata/programme.csv");
         const formData = new FormData();
         formData.append('file', file);
 
         await actions.uploadProgram({commit} as any, formData);
-
-        expect(commit.mock.calls[0][0]).toStrictEqual({
-            type: "ProgramError",
-            payload: "could not find function \"validate_func\""
-        });
+        expect(commit.mock.calls[0][0]["type"]).toBe("ProgramLoaded");
     });
 
     it("can upload anc", async () => {
 
         const commit = jest.fn();
 
-        const file = fs.createReadStream("fakefile");
+        const file = fs.createReadStream("../testdata/anc.csv");
         const formData = new FormData();
         formData.append('file', file);
 
         await actions.uploadANC({commit} as any, formData);
-
-        expect(commit.mock.calls[0][0]).toStrictEqual({
-            type: "ANCError",
-            payload: "cannot open the connection"
-        });
+        expect(commit.mock.calls[0][0]["type"]).toBe("ANCLoaded");
     });
 
 });
