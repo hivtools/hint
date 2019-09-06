911fb716289f2b9d1a028ebc6dd762429e3ec884
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-09-06 14:05:01 +0100

    Removed InternalResponse and made dummy data for newer SurveyResponse structure

--- src/app/static/src/app/types.ts
+++ src/app/static/src/app/types.ts
@@ -1,9 +1,7 @@
 import {Payload} from "vuex";
-import {ProgrammeResponse, SurveyResponse} from "./generated";
 
 export interface PayloadWithType<T> extends Payload {
     payload: T
 }
 
-export type InternalResponse = SurveyResponse | ProgrammeResponse
 
