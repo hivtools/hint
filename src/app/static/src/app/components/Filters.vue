84db3cfb633681f03db6dfcd5d77faf3f29782d3
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-09-06 11:01:46 +0100

    Add sex filter to component

--- src/app/static/src/app/components/Filters.vue
+++ src/app/static/src/app/components/Filters.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        {{selectedDataTypeString}}
+        <div>
+            <treeselect id="sex-filters" v-if="hasSelectedDataType" :multiple="true" :options="sexFilterOptions"
+                        v-on:select="sexFilterSelected" v-on:deselect="sexFilterDeselected"></treeselect>
+        </div>
     </div>
 </template>
 
@@ -8,16 +11,38 @@
     import Vue from "vue";
     import {mapActions, mapState} from "vuex";
     import {DataType, FilteredDataState} from "../store/filteredData/filteredData";
+    import Treeselect from '@riophae/vue-treeselect';
 
     const namespace: string = 'filteredData';
+
+    const treeselectOptions = (stringOptions: string[]) => {
+        return stringOptions.map(x => { return {"id": x, "label": x}  });
+    };
 
     export default Vue.extend ({
         name: "Filters",
         computed: mapState<FilteredDataState>(namespace, {
 
             selectedDataType: state => state.selectedDataType,
-            selectedDataTypeString: state => state.selectedDataType ? DataType[state.selectedDataType] : "none"
-        })
+            hasSelectedDataType: state => state.selectedDataType != null,
+
+            sexFilterOptions: state => treeselectOptions(
+                                            state.selectedDataType == DataType.ANC ?
+                                            ["female"] :
+                                            ["female", "male", "both"]
+            )
+        }),
+        methods: {
+            sexFilterSelected(value: object){
+                //TODO: raise filterAdded action
+                const option: string  = value.id;
+            },
+            sexFilterDeselected(value: object){
+                //TODO: raise filterRemoved action
+                const option: string  = value.id;
+            }
+        },
+        components: { Treeselect }
     });
 </script>
 
