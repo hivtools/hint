59d84e65b5f3ccf1476712d2fda0997f81280d03
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-09-12 17:55:28 +0100

    Fixed test

--- src/app/static/src/tests/filteredData/actions.test.ts
+++ src/app/static/src/tests/filteredData/actions.test.ts
@@ -5,9 +5,10 @@ describe("filteredData actions", () => {
     it("commits mutation on filterUpdated", async () => {
 
         const commit = jest.fn();
-        await actions.filterUpdated({commit} as any, [FilterType.Survey, ["survey1"]]);
+        const newFilters = [{id: "s1", name: "survey1"}];
+        await actions.filterUpdated({commit} as any, [FilterType.Survey, newFilters]);
 
-        expect(commit.mock.calls[0][0]).toStrictEqual({type: "FilterUpdated", payload: [FilterType.Survey, ["survey1"]]});
+        expect(commit.mock.calls[0][0]).toStrictEqual({type: "FilterUpdated", payload: [FilterType.Survey, newFilters]});
     });
 
 });
