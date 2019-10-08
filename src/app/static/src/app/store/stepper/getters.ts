6da69d0a39a2190c57f63c04ce933c14dede73a4
Alex <alex.hill@gmail.com>
2019-10-08 15:48:27 +0100

    make sure to make copy of initial state rather than referencing it directly

--- src/app/static/src/app/store/stepper/getters.ts
+++ src/app/static/src/app/store/stepper/getters.ts
@@ -16,7 +16,7 @@ export const getters: StepperGetters & GetterTree<StepperState, RootState> = {
             1: rootGetters['baseline/complete'] && rootGetters['metadata/complete'],
             2: rootGetters['surveyAndProgram/complete'],
             3: rootGetters['surveyAndProgram/complete'], // for now just mark as complete as soon as it's ready
-            4: rootState.modelRun.success,
+            4: rootState.modelRun.success && rootState.modelRun.errors.length == 0,
             5: false
         }
     }
