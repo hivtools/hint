0668b4c876f6ed7eea89c4a94ebadafc94b1a27d
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-10-08 12:10:21 +0100

    Added missing mapControl tests

--- src/app/static/src/tests/components/plots/mapControl.test.ts
+++ src/app/static/src/tests/components/plots/mapControl.test.ts
@@ -12,31 +12,40 @@ Vue.use(Vuex);
 
 describe("Map control component", () => {
 
-    const store = new Vuex.Store({
-        modules: {
-            filteredData: {
-                namespaced: true,
-                state: mockFilteredDataState({selectedDataType: DataType.Survey})
-            },
-            metadata: {
-                namespaced: true,
-                getters: {
-                    choroplethIndicatorsMetadata: () => {
-                        return {
-                            art_coverage: {
-                                name: "ART coverage"
-                            },
-                            prevalence: {
-                                name: "Prevalence"
-                            }
+    const getStore = (choroplethIndicatorsMetadata?: any, selectedDataType: DataType = DataType.Survey) => {
+
+        if (!choroplethIndicatorsMetadata) {
+            choroplethIndicatorsMetadata = {
+                art_coverage: {
+                    name: "ART coverage"
+                },
+                prevalence: {
+                    name: "Prevalence"
+                }
+            }
+        }
+
+        return new Vuex.Store({
+            modules: {
+                filteredData: {
+                    namespaced: true,
+                    state: mockFilteredDataState({selectedDataType: selectedDataType})
+                },
+                metadata: {
+                    namespaced: true,
+                    getters: {
+                        choroplethIndicatorsMetadata: () => {
+                            return choroplethIndicatorsMetadata
+
                         }
                     }
                 }
             }
-        }
-    });
+        });
+    };
 
     it("renders indicator options", () => {
+        const store = getStore();
         const wrapper = shallowMount(MapControl, {store, localVue});
 
         expect(wrapper.findAll(TreeSelect).at(0).props("options"))
@@ -44,7 +53,60 @@ describe("Map control component", () => {
                 {id: "art", label: "ART coverage"}]);
     });
 
+    it("renders indicator options with prevalence metadata only", () => {
+        const store = getStore({
+            prevalence: {
+                name: "Prevalence"
+            }
+        });
+        const wrapper = shallowMount(MapControl, {store, localVue});
+
+        expect(wrapper.findAll(TreeSelect).at(0).props("options"))
+            .toStrictEqual([{id: "prev", label: "Prevalence"}]);
+    });
+
+    it("renders indicator options with ART coverage metadata only", () => {
+        const store = getStore({
+            art_coverage: {
+                name: "ART coverage"
+            }
+        });
+        const wrapper = shallowMount(MapControl, {store, localVue});
+
+        expect(wrapper.findAll(TreeSelect).at(0).props("options"))
+            .toStrictEqual([{id: "art", label: "ART coverage"}]);
+    });
+
+    it("renders indicator options with Current ART metadata only", () => {
+        const store = getStore({
+            current_art: {
+                name: "Number on ART"
+            }
+        });
+        const wrapper = shallowMount(MapControl, {store, localVue});
+
+        expect(wrapper.findAll(TreeSelect).at(0).props("options"))
+            .toStrictEqual([{id: "art", label: "Number on ART"}]);
+    });
+
+    it("does not render ART for Output data type", () => {
+        const store = getStore(null, DataType.Output);
+        const wrapper = shallowMount(MapControl, {store, localVue});
+
+        expect(wrapper.findAll(TreeSelect).at(0).props("options"))
+            .toStrictEqual([{id: "prev", label: "Prevalence"}]);
+    });
+
+    it("does not render ART for ANC data type", () => {
+        const store = getStore(null, DataType.ANC);
+        const wrapper = shallowMount(MapControl, {store, localVue});
+
+        expect(wrapper.findAll(TreeSelect).at(0).props("options"))
+            .toStrictEqual([{id: "prev", label: "Prevalence"}]);
+    });
+
     it("renders detail options", () => {
+        const store = getStore();
         const wrapper = shallowMount(MapControl, {store, localVue});
 
         expect(wrapper.findAll(TreeSelect).at(1).props("options"))
@@ -57,12 +119,14 @@ describe("Map control component", () => {
     });
 
     it("emits indicator-changed event with indicator", () => {
+        const store = getStore();
         const wrapper = shallowMount(MapControl, {store, localVue});
         wrapper.findAll(TreeSelect).at(0).vm.$emit("input", "art");
         expect(wrapper.emitted("indicator-changed")[0][0]).toBe("art");
     });
 
     it("emits detail-changed event with detail", () => {
+        const store = getStore();
         const wrapper = shallowMount(MapControl, {store, localVue});
         wrapper.findAll(TreeSelect).at(1).vm.$emit("input", 3);
         expect(wrapper.emitted("detail-changed")[0][0]).toBe(3);
