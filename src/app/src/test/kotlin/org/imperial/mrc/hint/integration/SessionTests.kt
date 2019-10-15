d9c7b56554ba23e9416374017d38bd6a533bc5d0
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-10-15 16:37:06 +0100

    Added test for error response if hash does not exist

--- src/app/src/test/kotlin/org/imperial/mrc/hint/integration/SessionTests.kt
+++ src/app/src/test/kotlin/org/imperial/mrc/hint/integration/SessionTests.kt
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.EnumSource
 import org.springframework.boot.test.web.client.postForEntity
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 
 class SessionTests : SecureIntegrationTests() {
@@ -31,6 +32,15 @@ class SessionTests : SecureIntegrationTests() {
             val data = getResponseData(responseEntity)
             AssertionsForClassTypes.assertThat(data["pjnz"]["hash"].asText()).isEqualTo(hash)
         }
+    }
+
+    @ParameterizedTest
+    @EnumSource(IsAuthorized::class)
+    fun `set session files fails with invalid hash`(isAuthorized: IsAuthorized) {
+        val postEntity = getJsonEntity("badhash")
+
+        val responseEntity = testRestTemplate.postForEntity<String>("/session/files/", postEntity)
+        assertSecureWithError(isAuthorized, responseEntity, HttpStatus.INTERNAL_SERVER_ERROR, "OTHER_ERROR")
     }
 
     private fun getJsonEntity(hash: String): HttpEntity<String> {
--- src/app/src/test/kotlin/org/imperial/mrc/hint/unit/controllers/MetadataControllerTests.kt
+++ src/app/src/test/kotlin/org/imperial/mrc/hint/unit/controllers/MetadataControllerTests.kt
@@ -4,7 +4,6 @@ import com.nhaarman.mockito_kotlin.*
 import org.assertj.core.api.Assertions.assertThat
 import org.imperial.mrc.hint.APIClient
 import org.imperial.mrc.hint.controllers.MetadataController
-import org.imperial.mrc.hint.models.SuccessResponse
 import org.junit.jupiter.api.Test
 import org.springframework.http.ResponseEntity
 
