171bf75330a7f7df491a418210b907e3431fa622
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-10-24 13:27:23 +0100

    Add modelOutput module with getter for barchart plotting metadata

---
+++ src/app/static/src/app/store/modelOutput/modelOutput.ts
@@ -0,0 +1,116 @@
+import {Module} from "vuex";
+import {RootState} from "../../root";
+import {BarchartIndicator, Filter} from "../../types";
+import {FilterOption} from "../../generated";
+
+const namespaced: boolean = true;
+
+export interface ModelOutputState {}
+
+export const modelOutputGetters = {
+    barchartIndicators: (state: ModelOutputState, getters: any, rootState: RootState): BarchartIndicator[] => {
+        //TODO: Get these from metadata in ModelResultResponse
+        return [
+            {
+                "indicator": "art_coverage",
+                "value_column": "mean",
+                "indicator_column": "indicator_id",
+                "indicator_value": "4",
+                "name": "ART coverage",
+                "error_low_column": "lower",
+                "error_high_column": "upper"
+            },
+            {
+                "indicator": "current_art",
+                "value_column": "mean",
+                "indicator_column": "indicator_id",
+                "indicator_value": "5",
+                "name": "ART number",
+                "error_low_column": "lower",
+                "error_high_column": "upper"
+            },
+            {
+                "indicator": "incidence",
+                "value_column": "mean",
+                "indicator_column": "indicator_id",
+                "indicator_value": "6",
+                "name": "Incidence",
+                "error_low_column": "lower",
+                "error_high_column": "upper"
+            },
+            {
+                "indicator": "new_infections",
+                "value_column": "mean",
+                "indicator_column": "indicator_id",
+                "indicator_value": "7",
+                "name": "New Infections",
+                "error_low_column": "lower",
+                "error_high_column": "upper"
+            },
+            {
+                "indicator": "plhiv",
+                "value_column": "mean",
+                "indicator_column": "indicator_id",
+                "indicator_value": "3",
+                "name": "PLHIV",
+                "error_low_column": "lower",
+                "error_high_column": "upper"
+            },
+            {
+                "indicator": "population",
+                "value_column": "mean",
+                "indicator_column": "indicator_id",
+                "indicator_value": "1",
+                "name": "Population",
+                "error_low_column": "lower",
+                "error_high_column": "upper"
+            },
+            {
+                "indicator": "prevalence",
+                "value_column": "mean",
+                "indicator_column": "indicator_id",
+                "indicator_value": "2",
+                "name": "Prevalence",
+                "error_low_column": "lower",
+                "error_high_column": "upper"
+            }];
+    },
+    barchartFilters: (state: ModelOutputState, getters: any, rootState: RootState): Filter[] => {
+        //TODO: Get these from ModelResultResponse
+        const filterOptions = rootState.modelRun.result!!.filters!!;
+
+        const ageOptions = filterOptions.age || [];
+
+        const regions: FilterOption[] = rootState.baseline.shape!!.filters!!.regions ?
+                                        [rootState.baseline.shape!!.filters!!.regions] : [];
+
+        return [
+            {
+                "id": "age",
+                "column_id": "age_group",
+                "label": "Age group",
+                "options": ageOptions
+            },
+            {
+                "id": "region",
+                "column_id": "area_id",
+                "label": "Region",
+                "options": regions
+            },
+            {
+                "id": "sex",
+                "column_id": "sex",
+                "label": "Sex",
+                "options": [
+                    {"id": "female", "label": "female"},
+                    {"id": "male", "label": "male"}
+                 ]
+            }
+        ];
+    }
+};
+
+export const modelOutput: Module<ModelOutputState, RootState> = {
+    namespaced,
+    getters: modelOutputGetters
+};
---
+++ src/app/static/src/tests/modelOutput/modelOutput.test.ts
@@ -0,0 +1,67 @@
+import {modelOutputGetters} from "../../app/store/modelOutput/modelOutput";
+import {
+    mockBaselineState,
+    mockModelResultResponse, mockModelRunState,
+    mockRootState, mockShapeResponse
+} from "../mocks";
+
+
+describe("modelOutput module", () => {
+    it("gets barchart indicators", async () => {
+        const result = modelOutputGetters.barchartIndicators({}, null, mockRootState());
+        expect(result.length).toEqual(7);
+    });
+
+    it("gets barchart filters", async () => {
+        const rootState = mockRootState({
+            baseline: mockBaselineState({
+                shape: mockShapeResponse({
+                    filters: {
+                        regions: {
+                            label: "label 1",
+                            id: "id1",
+                            children: []
+                        }
+                    }
+                })
+            }),
+            modelRun: mockModelRunState({
+                result: mockModelResultResponse({
+                    filters: {
+                        age: [ {id: "1", label: "0-4"}],
+                        quarter: [],
+                        indicators: []
+                    }
+                })
+            })
+        });
+
+        const result = modelOutputGetters.barchartFilters({}, null, rootState);
+        expect(result.length).toEqual(3);
+        expect (result[0]).toStrictEqual({
+            id: "age",
+            column_id: "age_group",
+            label: "Age group",
+            options: [
+                {id: "1", label: "0-4"}
+            ]
+        });
+        expect (result[1]).toStrictEqual({
+            id: "region",
+            column_id: "area_id",
+            label: "Region",
+            options: [
+                {id: "id1", label: "label 1", children: []}
+            ]
+        });
+        expect (result[2]).toStrictEqual({
+            id: "sex",
+            column_id: "sex",
+            label: "Sex",
+            options: [
+                {id: "female", label: "female"},
+                {id: "male", label: "male"}
+            ]
+        });
+    });
+});
