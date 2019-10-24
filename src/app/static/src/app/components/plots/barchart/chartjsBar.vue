e82719d02f82c5bd95958e6c53cdff0efd29d8f4
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-10-24 14:52:26 +0100

    Added chartjsBar component

---
+++ src/app/static/src/app/components/plots/barchart/chartjsBar.vue
@@ -0,0 +1,62 @@
+<script lang="ts">
+    import Vue from "vue";
+    import { Bar } from 'vue-chartjs'
+    import ErrorBarsPlugin from 'chartjs-plugin-error-bars'
+    export default Vue.extend({
+        extends: Bar,
+        name: "ChartjsBar",
+        props: {
+            chartData: {
+                type: Object,
+                default: null
+            },
+            xLabel: String,
+            yLabel: String
+        },
+        methods: {
+            updateRender: function(){
+                (this as any).addPlugin(ErrorBarsPlugin as any);
+                (this as any).renderChart(this.chartData, {
+                    scales: {
+                        yAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: this.yLabel
+                            },
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }],
+                        xAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: this.xLabel
+                            }
+                        }]
+                    },
+                    legend: {
+                        position: "right",
+                    },
+                    responsive:true,
+                    maintainAspectRatio: false,
+                    plugins: {
+                        chartJsPluginErrorBars: {
+                            color: '#000',
+                            width: '2px',
+                            lineWidth: '2px',
+                            absoluteValues: true
+                        }
+                    }
+                })
+            }
+        },
+        mounted () {
+            this.updateRender();
+        },
+        watch: {
+            chartData: function(newVal) {
+                this.updateRender();
+            }
+        }
+    });
+</script>
---
+++ src/app/static/src/tests/components/plots/barchart/chartjsBar.test.ts
@@ -0,0 +1,55 @@
+import ChartjsBar from "../../../../app/components/plots/barchart/chartjsBar.vue";
+import {createLocalVue, shallowMount} from "@vue/test-utils";
+import Vue from "vue";
+import Vuex from "vuex";
+
+const localVue = createLocalVue();
+Vue.use(Vuex);
+
+describe("chartjsBar component", () => {
+
+    const propsData = {
+        xLabel: "X Axis",
+        yLabel: "Y Axis",
+        chartData: {
+            labels: ["group1", "group2"],
+            datasets:[
+                {
+                    label: "dataset1",
+                    backgroundColor: "#111111",
+                    data: [1,2],
+                    errorBars: {
+                        "group1": {plus: 1.1, minus: 0.9},
+                        "group2": {plus: 2.1, minus: 1.9}
+                    }
+                }
+            ]
+        }
+    };
+
+    it("renders as expected", () => {
+        const wrapper = shallowMount(ChartjsBar, {propsData, localVue});
+        expect(wrapper.findAll("canvas").length).toEqual(1);
+        console.log(wrapper.html());
+    });
+
+    it("updates render when props chartData changes", () => {
+        const wrapper = shallowMount(ChartjsBar, {propsData, localVue});
+
+        const mockUpdate = jest.fn();
+        const vm = (wrapper as any).vm;
+        vm.updateRender = mockUpdate;
+
+        const newChartData = {
+            ...propsData.chartData,
+            labels: ["group1"]
+        };
+        wrapper.setProps({
+            ...propsData,
+            chartData: newChartData
+        });
+
+        expect(mockUpdate.mock.calls.length).toBe(1);
+
+    });
+});
