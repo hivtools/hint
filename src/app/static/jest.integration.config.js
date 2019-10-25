78b912c496fdb18f2751783a0776b2c1c5d47830
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-10-25 17:06:56 +0100

    Added getProcessedData test

--- src/app/static/jest.integration.config.js
+++ src/app/static/jest.integration.config.js
@@ -1,5 +1,6 @@
 var config = require('./jest.config');
 
+config.setupFiles = ["./src/tests/setup.ts"];
 config.testRegex = "(/__tests__/.*|(\\.|/)(itest))\\.[jt]sx?$";
 config.globals = {...config.globals, "appUrl": "http://localhost:8080/"};
 config.coverageDirectory = "./coverage/integration/";
