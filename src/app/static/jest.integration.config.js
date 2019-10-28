d7b242289a3e37fdb591233785dc54ef5446ca4a
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-10-28 14:33:55 +0000

    revert unnecessary integration test config change

--- src/app/static/jest.integration.config.js
+++ src/app/static/jest.integration.config.js
@@ -1,6 +1,5 @@
 var config = require('./jest.config');
 
-config.setupFiles = ["./src/tests/setup.ts"];
 config.testRegex = "(/__tests__/.*|(\\.|/)(itest))\\.[jt]sx?$";
 config.globals = {...config.globals, "appUrl": "http://localhost:8080/"};
 config.coverageDirectory = "./coverage/integration/";
