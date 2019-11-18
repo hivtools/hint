0bf3a0ba3be45b850af89f68cf688f0d1f9928a6
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-11-18 13:44:49 +0000

    Add workaround for quarter bug in barchart plotting metadata

--- src/app/static/src/app/store/modelOutput/modelOutput.ts
+++ src/app/static/src/app/store/modelOutput/modelOutput.ts
@@ -16,8 +16,27 @@ export const modelOutputGetters = {
         const regions: FilterOption[] = rootState.baseline.shape!!.filters!!.regions ?
                                         [rootState.baseline.shape!!.filters!!.regions] : [];
 
+        let filters = rootState.modelRun.result!!.plottingMetadata.barchart.filters;
+        //THIS IS A WORKAROUND FOR A BUG IN HINTR - take out when fixed!
+        //Column_id and label are the wrong way round for quarter
+        //(Also, sex is missing, leaving that out for now)
+        //Should get fixed as part of https://vimc.myjetbrains.com/youtrack/issue/mrc-577
+        let newFilters = null;
+        const quarter = filters.find((f: any) => f.id=="quarter");
+        if (quarter) {
+            const newQuarter = {
+                ...quarter,
+                column_id:  "quarter_id",
+                label: "Quarter"
+            };
+            newFilters = filters.filter((f: any) => f.id != "quarter");
+            newFilters.push(newQuarter);
+
+            filters = newFilters;
+        }
+
         return  [
-            ...rootState.modelRun.result!!.plottingMetadata.barchart.filters,
+            ...filters,
             {
                 "id": "region",
                 "column_id": "area_id",
