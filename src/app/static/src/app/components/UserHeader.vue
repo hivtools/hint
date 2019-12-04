f4a2a477369349eeef71659bc781fd75e35d019e
Robert Ashton <r.ashton@imperial.ac.uk>
2019-12-04 15:45:00 +0000

    Add margin and right divider

--- src/app/static/src/app/components/UserHeader.vue
+++ src/app/static/src/app/components/UserHeader.vue
@@ -23,7 +23,7 @@
                         <input type="file" style="display: none;" ref="loadFile" v-on:change="load" accept=".json">
                     </div>
                 </div>
-                <a href="https://forms.gle/QxCT1b4ScLqKPg6a7" target="_blank" style="margin-right: 10px">Report a bug</a>
+                <a href="https://forms.gle/QxCT1b4ScLqKPg6a7" target="_blank" class="pr-2 mr-2 border-right">Report a bug</a>
                 <a href="/logout">Logout</a>
             </div>
         </nav>
