6fb71e4d4af03dda4c3d3d82b17363b16d8b17cb
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-12-10 13:43:31 +0000

    Retain what state we can on reset

--- src/app/static/src/app/store/root/actions.ts
+++ src/app/static/src/app/store/root/actions.ts
@@ -1,6 +1,7 @@
 import {ActionContext, ActionTree} from "vuex";
 import {RootState} from "../../root";
 import {StepDescription} from "../stepper/stepper";
+import {RootMutation} from "./mutations";
 
 export interface RootActions {
     validate: (store: ActionContext<RootState, RootState>) => void;
@@ -17,11 +18,25 @@ export const actions: ActionTree<RootState, RootState> & RootActions = {
             .filter((i: number) => i < maxCompleteOrActive && !completeSteps.includes(i));
 
         if (invalidSteps.length > 0) {
-            await Promise.all([
-                dispatch("baseline/deleteAll"),
-                dispatch("surveyAndProgram/deleteAll")
-            ]);
-            commit({type: "Reset"});
+
+            //Invalidate any steps which come after the first invalid step
+            const maxValidStep = Math.min(...invalidSteps) - 1;
+
+            const promises = [];
+
+            if (maxValidStep < 1){
+                promises.push(dispatch("baseline/deleteAll"));
+            }
+            if (maxValidStep < 2){
+                promises.push(dispatch("surveyAndProgram/deleteAll"));
+            }
+
+            if (promises.length > 0) {
+                await Promise.all(promises);
+            }
+
+            commit({type: RootMutation.Reset, payload: maxValidStep});
+            commit({type: RootMutation.ResetFilteredDataSelections});
         }
     }
 };
--- src/app/static/src/app/store/root/mutations.ts
+++ src/app/static/src/app/store/root/mutations.ts
@@ -1,11 +1,19 @@
 import {MutationTree} from "vuex";
 import {emptyState, RootState} from "../../root";
-import {DataType} from "../filteredData/filteredData";
+import {DataType, initialFilteredDataState} from "../filteredData/filteredData";
 import {initialModelOptionsState} from "../modelOptions/modelOptions";
 import {initialModelRunState} from "../modelRun/modelRun";
 import {initialModelOutputState} from "../modelOutput/modelOutput";
 import {initialPlottingSelectionsState} from "../plottingSelections/plottingSelections";
 import {data} from "../../../tests/components/plots/barchart/utils.test";
+import {initialLoadState, LoadState} from "../load/load";
+import {PayloadWithType} from "../../types";
+import {Error} from "../../generated";
+import {initialMetadataState} from "../metadata/metadata";
+import {initialSurveyAndProgramDataState} from "../surveyAndProgram/surveyAndProgram";
+import {initialStepperState} from "../stepper/stepper";
+import {initialErrorsState} from "../errors/errors";
+import {initialBaselineState} from "../baseline/baseline";
 
 export enum RootMutation {
     Reset = "Reset",
@@ -15,9 +23,30 @@ export enum RootMutation {
 }
 
 export const mutations: MutationTree<RootState> = {
-    [RootMutation.Reset](state: RootState) {
+    [RootMutation.Reset](state: RootState, action: PayloadWithType<number>) {
 
-        Object.assign(state, emptyState());
+        const maxValidStep = action.payload;
+
+        Object.assign(state, {
+            version: state.version,
+            baseline: maxValidStep < 1 ? initialBaselineState() : state.baseline,
+            metadata: maxValidStep < 1 ? initialMetadataState() : state.metadata,
+            surveyAndProgram: maxValidStep < 2 ? initialSurveyAndProgramDataState() : state.surveyAndProgram,
+            modelOptions: maxValidStep < 3 ? initialModelOptionsState() : state.modelOptions,
+            modelOutput: maxValidStep <= 6 ? initialModelOutputState() :  state.modelOutput,
+            modelRun: maxValidStep <= 6 ? initialModelRunState() :  state.modelRun,
+            plottingSelections: maxValidStep <= 6 ? initialPlottingSelectionsState() : state.plottingSelections,
+            filteredData: state.filteredData,
+            stepper: state.stepper,
+            load: state.load,
+            errors: state.errors
+        });
+
+        //THIS IS A BIT MESSY - WE'RE ASSUMING THE MAX WE CAN GO HERE IS 4, BUT NOT ABOVE
+        const maxAccessibleStep = maxValidStep < 4 ? maxValidStep : 4;
+        if (state.stepper.activeStep > maxAccessibleStep) {
+            state.stepper.activeStep = maxAccessibleStep;
+        }
 
         state.surveyAndProgram.ready = true;
         state.baseline.ready = true;
