0fc944ee6661df88f8b05808ab24f8c54d74c35c
Alex <alex.hill@gmail.com>
2019-12-16 17:36:28 +0000

    test page templates pass props

---
+++ src/app/static/src/tests/components/password/forgotPasswordPage.test.ts
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const html = fs.readFileSync(path.resolve(__dirname, '../../../../templates/forgot-password.ftl'), 'utf8');
+
+document.documentElement.innerHTML = html.toString();
+document.getElementById("app")!!.setAttribute("title", "AppTitle");
+
+import {forgotPasswordApp} from "../../../app/forgotPassword";
+
+describe('forgot password page', function () {
+
+    it("passes title to app", () => {
+        let c = forgotPasswordApp.$options;
+        expect(document.body.getElementsByClassName("navbar-header").item(0)!!.textContent!!.trim())
+            .toBe("AppTitle")
+    });
+
+});
