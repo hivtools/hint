87495abb18a3208cb332bda15e8108d78ff193ee
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-01-07 14:21:33 +0000

    Fixed wrong file update

--- src/app/static/src/tests/components/plots/bubble/bubblePlot.test.ts
+++ src/app/static/src/tests/components/plots/bubble/bubblePlot.test.ts
@@ -1,201 +1,182 @@
-import {
-    getFeatureIndicators,
-    getIndicatorRanges,
-    getRadius,
-    toIndicatorNameLookup
-} from "../../../../app/components/plots/bubble/utils";
+import {createLocalVue, shallowMount, Wrapper} from "@vue/test-utils";
+import Vue from 'vue';
+import BubblePlot from "../../../../app/components/plots/bubble/BubblePlot.vue";
+import {LGeoJson, LCircleMarker, LTooltip} from "vue2-leaflet";
+import {getFeatureIndicators, getIndicatorRanges, getRadius} from "../../../../app/components/plots/bubble/utils";
 import {getColor} from "../../../../app/store/filteredData/utils";
+import MapControl from "../../../../app/components/plots/MapControl.vue";
 
-describe("Bubble plot utils", () => {
+const localVue = createLocalVue();
 
-    const indicators = [
+const propsData = {
+    features: [
         {
-            indicator: "plhiv", value_column: "plhiv", name: "PLHIV", min: 0, max:0, colour: "interpolateGreys", invert_scale: false
-        },
-        {
-            indicator: "prevalence", value_column: "prevalence", name: "Prevalence", min: 0, max: 0, colour: "interpolateGreys", invert_scale: false
-        }
-    ];
-
-    const indicatorRanges = {
-        plhiv: {min: 1, max: 100},
-        prevalence: {min: 0, max: 0.8}
-    };
-
-    const selectedFeatureIds = ["MWI_1_1", "MWI_1_2"];
-
-    const minRadius = 10;
-    const maxRadius = 1000;
-
-    const expectedFeatureIndicators = {
-        MWI_1_1: {
-            plhiv: {
-                value: 12,
-                color: getColor(12, indicators[0]),
-                radius: getRadius(12, 1, 100, minRadius, maxRadius)
-            },
-            prevalence: {
-                value: 0.5,
-                color: getColor(0.5, indicators[1]),
-                radius: getRadius(0.5, 0, 0.8, minRadius, maxRadius)
+            "type": "Feature",
+            "properties": {"iso3": "MWI", "area_id": "MWI_3_1", "area_name": "North", "area_level": 3, "center_x": 35.7082, "center_y": -15.2046},
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [[[[35.7083, -15.2047], [35.7117, -15.2066], [35.7108, -15.2117]]]]
             }
         },
-        MWI_1_2: {
-            plhiv: {
-                value: 14,
-                color: getColor(14, indicators[0]),
-                radius: getRadius(14, 1, 100, minRadius, maxRadius)
-            },
-            prevalence: {
-                value: 0.6,
-                color: getColor(0.6, indicators[1]),
-                radius: getRadius(0.6, 0, 0.8, minRadius, maxRadius)
+        {
+            "type": "Feature",
+            "properties": {"iso3": "MWI", "area_id": "MWI_4_1", "area_name": "North West", "area_level": 4, "center_x": 35.7083, "center_y": -15.2047},
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [[[[35.7083, -15.2047], [35.7117, -15.2066], [35.7108, -15.2117]]]]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {"iso3": "MWI", "area_id": "MWI_4_2", "area_name": "North East", "area_level": 4, "center_x": 35.7084, "center_y": -15.2048},
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [[[[35.7083, -15.2047], [35.7117, -15.2066], [35.7108, -15.2117]]]]
             }
         }
-    };
+    ],
+    featureLevels: [
+        {id: 3, display: true, area_level_label: "Admin Level 3"},
+        {id: 4, display: true, area_level_label: "Admin Level 4"}
+    ],
+    chartdata: [
+        {
+            area_id: "MWI_3_1", plhiv: 1, prevalence: 0.01
+        },
+        {
+            area_id: "MWI_4_1", plhiv: 10, prevalence: 0.1
+        },
+        {
+            area_id: "MWI_4_2", plhiv: 20, prevalence: 0.2
+        },
+    ],
+    indicators: [
+        {
+            indicator: "plhiv", value_column: "plhiv", name: "PLHIV", min: 1, max: 100, colour: "interpolateGreys", invert_scale: false
+        },
+        {
+            indicator: "prevalence", value_column: "prevalence", name: "Prevalence", min: 0, max: 0.8, colour: "interpolateGreys", invert_scale: false
+        }
+    ]
+};
 
-    it("can get indicator ranges", () => {
-        const data = [
-            {area_id: "MWI_1_1", prevalence: 0.5, plhiv: 15},
-            {area_id: "MWI_1_2", prevalence: 0.6, plhiv: 14},
-            {area_id: "MWI_1_3", prevalence: 0.7, plhiv: 13}
-        ];
+const getWrapper  = () => {
+    return shallowMount(BubblePlot, {propsData, localVue});
+};
 
-        const result = getIndicatorRanges(data, indicators);
+describe("BubblePlot component", () => {
+    it("renders as expected", () => {
+        const wrapper = getWrapper();
+        const geoJsons = wrapper.findAll(LGeoJson);
+        expect(geoJsons.length).toBe(2);
+        expect(geoJsons.at(0).props().geojson).toBe(propsData.features[1]);
+        expect(geoJsons.at(1).props().geojson).toBe(propsData.features[2]);
 
-        expect(result).toStrictEqual({
-            plhiv: {min: 13, max: 15},
-            prevalence: {min: 0.5, max: 0.7}
+        //These are hardcoded in the component
+        const minRadius = 10;
+        const maxRadius = 60;
+
+        const circles = wrapper.findAll(LCircleMarker);
+        expect(circles.length).toBe(2);
+        expect(circles.at(0).props().latLng).toEqual([-15.2047, 35.7083]);
+        expect(circles.at(0).props().radius).toEqual(getRadius(10, 1, 20, 10, 60));
+        expect(circles.at(0).find(LTooltip).props().content).toEqual(`<div>
+                            <strong>North West</strong>
+                            <br/>Prevalence: 0.1
+                            <br/>PLHIV: 10
+                        </div>`);
+        let color = getColor(0.1, propsData.indicators[1]);
+        expect(circles.at(0).props().color).toEqual(color);
+        expect(circles.at(0).props().fillColor).toEqual(color);
+
+        expect(circles.at(1).props().latLng).toEqual([-15.2048, 35.7084]);
+        expect(circles.at(1).props().radius).toEqual(getRadius(20, 1, 20, 10, 60));
+        expect(circles.at(1).find(LTooltip).props().content).toEqual(`<div>
+                            <strong>North East</strong>
+                            <br/>Prevalence: 0.2
+                            <br/>PLHIV: 20
+                        </div>`);
+        color = getColor(0.2, propsData.indicators[1]);
+        expect(circles.at(1).props().color).toEqual(color);
+        expect(circles.at(1).props().fillColor).toEqual(color);
+
+        expect(wrapper.find(MapControl).props().initialDetail).toEqual(4);
+        expect(wrapper.find(MapControl).props().showIndicators).toEqual(false);
+    });
+
+    it("computes indicatorRanges", () => {
+        const wrapper = getWrapper();
+        const vm = wrapper.vm as any;
+
+        expect(vm.indicatorRanges).toStrictEqual(getIndicatorRanges(propsData.chartdata, propsData.indicators));
+    });
+
+    it("computes featureIndicators", () => {
+        const wrapper = getWrapper();
+        const vm = wrapper.vm as any;
+
+        expect(vm.featureIndicators).toStrictEqual(getFeatureIndicators(propsData.chartdata,
+            [propsData.features[1] as any, propsData.features[2] as any],
+            propsData.indicators,
+            getIndicatorRanges(propsData.chartdata, propsData.indicators),
+            10,
+            60));
+    });
+
+    it("computes featuresByLevel", () => {
+        const wrapper = getWrapper();
+        const vm = wrapper.vm as any;
+
+        expect(vm.featuresByLevel).toStrictEqual({
+            3: [propsData.features[0]],
+            4: [propsData.features[1], propsData.features[2]]
         });
     });
 
-    it("can get feature indicators from wide format data", () => {
-        const data = [
-            {area_id: "MWI_1_1", prevalence: 0.5, plhiv: 12},
-            {area_id: "MWI_1_2", prevalence: 0.6, plhiv: 14},
-            {area_id: "MWI_1_3", prevalence: 0.7, plhiv: 16} //should not be included, not in selectedFeatures
-        ];
-
-        const result = getFeatureIndicators(data, selectedFeatureIds, indicators, indicatorRanges, [], {},
-            minRadius, maxRadius);
-
-        expect(result).toStrictEqual(expectedFeatureIndicators);
+    it("computes maxLevel", () => {
+        const wrapper = getWrapper();
+        const vm = wrapper.vm as any;
+        expect(vm.maxLevel).toBe(4);
     });
 
-    it("can get feature indicators from long format data", () => {
-        const longIndicators = [
-            {
-                indicator: "plhiv", value_column: "value", indicator_column: "indicator", indicator_value: "plhiv",
-                name: "PLHIV", min: 0, max: 0, colour: "interpolateGreys", invert_scale: false
-            },
-            {
-                indicator: "prevalence", value_column: "value", indicator_column: "indicator", indicator_value: "prev",
-                name: "Prevalence", min: 0, max: 0, colour: "interpolateGreys", invert_scale: false
-            }
-        ];
+    it("computes currentFeatures", () => {
+        const wrapper = getWrapper();
+        const vm = wrapper.vm as any;
 
-        const data = [
-            {area_id: "MWI_1_1", indicator: "plhiv", value: 12},
-            {area_id: "MWI_1_1", indicator: "prev", value: 0.5},
-            {area_id: "MWI_1_2", indicator: "plhiv", value: 14},
-            {area_id: "MWI_1_2", indicator: "prev", value: 0.6},
-            {area_id: "MWI_1_3", indicator: "plhiv", value: 14} //should not be included, not in selectedFeatures
-        ];
-
-        const result = getFeatureIndicators(data, selectedFeatureIds, longIndicators, indicatorRanges,[],{},
-            minRadius, maxRadius);
-        expect(result).toStrictEqual(expectedFeatureIndicators);
+        expect(vm.currentFeatures).toStrictEqual([propsData.features[1], propsData.features[2]]);
     });
 
-    it("can exclude rows based on filters", () => {
-        const filters = [
-            {id: "age", label: "Age", column_id: "age", options: []},
-            {id: "sex", label: "Sex", column_id: "sex", options: []}
-        ];
-
-        const selectedFilterValues = {
-            age: [{id: "0:15", label: "0-15"}, {id: "15:30", label: "15-30"}],
-            sex: [{id: "female", label: "Female"}]
-        };
-
-        const data = [
-            {area_id: "MWI_1_1", prevalence: 0.5, plhiv: 12, sex: "female", age: "0:15"},
-            {area_id: "MWI_1_2", prevalence: 0.6, plhiv: 14, sex: "female", age: "15:30"},
-            {area_id: "MWI_1_3", prevalence: 0.7, plhiv: 16, sex: "female", age: "0:15"}, //not included: not in selectedFeatures
-            {area_id: "MWI_1_1", prevalence: 0.1, plhiv: 18, sex: "male", age: "0:15"}, //not included: no sex filter match
-            {area_id: "MWI_1_1", prevalence: 0.2, plhiv: 20, sex: "female", age: "30:45"}, //not included: no age filter match
-        ];
-
-        const result = getFeatureIndicators(data, selectedFeatureIds, indicators, indicatorRanges, filters,
-            selectedFilterValues, minRadius, maxRadius);
-
-        expect(result).toStrictEqual(expectedFeatureIndicators);
-    });
-
-    it("can get feature indicators from partial rows", () => {
-        const partialData = [
-            {area_id: "MWI_1_1", prevalence: 0.5},
-            {area_id: "MWI_1_2", plhiv: 14},
-        ];
-
-        const result = getFeatureIndicators(partialData, selectedFeatureIds, indicators, indicatorRanges, [], {},
-            minRadius, maxRadius);
-
-        expect(result).toStrictEqual({
-            MWI_1_1: {
-                prevalence: {
-                    value: 0.5,
-                    color: getColor(0.5, indicators[1]),
-                    radius: getRadius(0.5, 0, 0.8, minRadius, maxRadius)
-                }
-            },
-            MWI_1_2: {
-                plhiv: {
-                    value: 14,
-                    color: getColor(14, indicators[0]),
-                    radius: getRadius(14, 1, 100, minRadius, maxRadius)
-                }
-            }
-        });
-    });
-
-    it("can get indicator name lookup", () => {
-        expect(toIndicatorNameLookup(indicators)).toStrictEqual({
+    it("computes indicatorNameLookup", () => {
+        const wrapper = getWrapper();
+        const vm = wrapper.vm as any;
+        expect(vm.indicatorNameLookup).toStrictEqual({
             plhiv: "PLHIV",
             prevalence: "Prevalence"
         });
     });
 
+    it("updateBounds updates bounds of map from features geojson", () => {
+        const wrapper = getWrapper();
+        const mockMapFitBounds = jest.fn();
 
-    const radiusToArea = (r: number) => {return Math.PI * r *r};
-    const areaToRadius = (x: number) => {return Math.sqrt(x/Math.PI)};
-    const minArea = radiusToArea(5);
-    const maxArea = radiusToArea(10);
-    it ("can get radius", () => {
-        expect(getRadius(1, 1, 10, 5, 10)).toBe(5);
-        expect(getRadius(10, 1, 10, 5, 10)).toBe(10);
+        const vm = wrapper.vm as any;
+        vm.$refs.map = {
+            fitBounds: mockMapFitBounds
+        };
 
-        const expectedArea = minArea + ((4/9) * (maxArea - minArea));
-        const expectedRadius = areaToRadius(expectedArea);
-        expect(getRadius(5, 1, 10, 5, 10)).toBeCloseTo(expectedRadius, 5);
+        //NB we're not updating to selected features yet, just to current level
+        vm.updateBounds();
+        expect(mockMapFitBounds.mock.calls[0][0]).toStrictEqual(
+            [
+                {_northEast: {lat: -15.2047, lng: 35.7117}, _southWest: {lat: -15.2117, lng: 35.7083}},
+                {_northEast: {lat: -15.2047, lng: 35.7117}, _southWest: {lat: -15.2117, lng: 35.7083}}
+         ]);
     });
 
-    it("can get radius where min value is 0", () => {
-        expect(getRadius(0, 0, 10, 5, 10)).toBe(5);
-        expect(getRadius(10, 0, 10, 5, 10)).toBe(10);
-
-        const expectedArea = minArea + (0.5 * (maxArea - minArea));
-        const expectedRadius = areaToRadius(expectedArea);
-        expect(getRadius(5, 0, 10, 5, 10)).toBeCloseTo(expectedRadius, 5);
-    });
-
-    it ("can get radius where min area is 0", () => {
-        expect(getRadius(0, 0, 10, 0, 10)).toBe(0);
-        expect(getRadius(10, 0, 10, 0, 10)).toBe(10);
-
-        const expectedArea = 0.5 * maxArea;
-        const expectedRadius = areaToRadius(expectedArea);
-        expect(getRadius(5, 0, 10, 0, 10)).toBeCloseTo(expectedRadius, 5);
-    });
-
+    it("updates detail", () => {
+        const wrapper = getWrapper();
+        const vm = wrapper.vm as any;
+        vm.onDetailChange(3);
+        expect(vm.detail).toBe(3);
+    })
 });
