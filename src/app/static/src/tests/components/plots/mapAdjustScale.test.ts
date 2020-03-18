1c96f8bfe68ef217175511451dfd99d25b671697
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-03-18 11:15:47 +0000

    Added update testss for Map adjust scale component

--- src/app/static/src/tests/components/plots/mapAdjustScale.test.ts
+++ src/app/static/src/tests/components/plots/mapAdjustScale.test.ts
@@ -1,6 +1,7 @@
-import {mount, shallowMount, WrapperArray} from '@vue/test-utils';
+import {mount, shallowMount} from '@vue/test-utils';
 import MapAdjustScale from "../../../app/components/plots/MapAdjustScale.vue";
 import {ColourScaleType} from "../../../app/store/colourScales/colourScales";
+import Vue from "vue";
 
 describe("MapAdjustScale component", () => {
 
@@ -48,5 +49,65 @@ describe("MapAdjustScale component", () => {
         expect((wrapper.find("#custom-min-input").element as HTMLInputElement).value).toBe("0");
         expect((wrapper.find("#custom-max-input").element as HTMLInputElement).disabled).toBe(false);
         expect((wrapper.find("#custom-max-input").element as HTMLInputElement).value).toBe("1");
+    });
+
+    it("emits update event when type changes", () => {
+        const wrapper = mount(MapAdjustScale, {propsData: {
+                show: true,
+                colourScale: {
+                    type: ColourScaleType.Default,
+                    customMin: 0,
+                    customMax: 1
+                }
+            }
+        });
+
+        wrapper.find("#type-input-custom").trigger("click");
+
+        expect(wrapper.emitted("update").length).toBe(1);
+        expect(wrapper.emitted("update")[0][0]).toStrictEqual({
+            type: ColourScaleType.Custom,
+            customMin: 0,
+            customMax: 1
+        });
+
+        wrapper.find("#type-input-default").trigger("click");
+
+        expect(wrapper.emitted("update").length).toBe(2);
+        expect(wrapper.emitted("update")[1][0]).toStrictEqual({
+            type: ColourScaleType.Default,
+            customMin: 0,
+            customMax: 1
+        });
+    });
+
+    it("emits update event when custom min or max changes", () => {
+        const wrapper = mount(MapAdjustScale, {propsData: {
+                show: true,
+                colourScale: {
+                    type: ColourScaleType.Custom,
+                    customMin: 0,
+                    customMax: 1
+                }
+            }
+        });
+
+        wrapper.find("#custom-min-input").setValue("0.5");
+        wrapper.find("#custom-min-input").trigger("change");
+        expect(wrapper.emitted("update").length).toBe(1);
+        expect(wrapper.emitted("update")[0][0]).toStrictEqual({
+            type: ColourScaleType.Custom,
+            customMin: 0.5,
+            customMax: 1
+        });
+
+        wrapper.find("#custom-max-input").setValue("1.5");
+        wrapper.find("#custom-max-input").trigger("change");
+        expect(wrapper.emitted("update").length).toBe(2);
+        expect(wrapper.emitted("update")[1][0]).toStrictEqual({
+            type: ColourScaleType.Custom,
+            customMin: 0.5,
+            customMax: 1.5
+        });
     });
 });
