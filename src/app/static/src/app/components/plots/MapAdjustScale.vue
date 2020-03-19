144702a25a71e3ddec8981a03e25240669169b0a
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-03-19 16:59:57 +0000

    Style improvements

--- src/app/static/src/app/components/plots/MapAdjustScale.vue
+++ src/app/static/src/app/components/plots/MapAdjustScale.vue
@@ -1,6 +1,6 @@
 import {ColourScaleType} from "../../store/colourScales/colourScales";
 <template>
-    <div v-if="show" class="p-3">
+    <div v-if="show" class="pt-2 pl-3">
         <div class="form-check">
             <label class="form-check-label">
                 <input id="type-input-default" class="form-check-input" type="radio" name="scaleType" :value="ColourScaleType.Default"
@@ -8,26 +8,31 @@ import {ColourScaleType} from "../../store/colourScales/colourScales";
                 Default
             </label>
         </div>
-
-        <div class="form-check">
+        <div class="form-check mt-1">
             <label class="form-check-label">
                 <input id="type-input-custom" class="form-check-input" type="radio" name="scaleType" :value="ColourScaleType.Custom"
                        v-model="colourScaleToAdjust.type" @change="update">
                 Custom
             </label>
         </div>
-        <div class="ml-2">
-            <div class="mt-2">
-                <label for="custom-min-input">Min</label>
-                <input id="custom-min-input" type="number" v-model.number="colourScaleToAdjust.customMin" @change="update" @keyup="update"
-                       :disabled="disableCustom">
+
+        <div class="mt-2 ml-2">
+            <div class="row p-0 mb-2">
+                <label for="custom-min-input" class="col col-form-label col-2">Min</label>
+                <div class="col pt-1">
+                    <input id="custom-min-input" type="number" step="any" v-model.number="colourScaleToAdjust.customMin"
+                           @change="update" @keyup="update" :disabled="disableCustom">
+                </div>
             </div>
-            <div class="mt-2">
-                <label for="custom-max-input">Max</label>
-                <input id="custom-max-input" type="number" v-model.number="colourScaleToAdjust.customMax" @change="update" @keyup="update"
-                       :disabled="disableCustom">
+            <div class="row">
+                <label class="col col-form-label col-2" for="custom-max-input">Max</label>
+                <div class="col pt-1">
+                    <input id="custom-max-input" type="number" step="any" v-model.number="colourScaleToAdjust.customMax"
+                           @change="update" @keyup="update" :disabled="disableCustom">
+                </div>
             </div>
         </div>
+        <div class="scale-error">{{invalidMsg}}</div>
     </div>
 </template>
 
