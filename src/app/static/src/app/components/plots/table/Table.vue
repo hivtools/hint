886658a7b7ecd1e95efaac8a0f507658d5559c99
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-07-03 15:29:32 +0100

    Construct array of objects using iterateDataValues

--- src/app/static/src/app/components/plots/table/Table.vue
+++ src/app/static/src/app/components/plots/table/Table.vue
@@ -12,13 +12,15 @@
     </ul> -->
     <h3>Table</h3>
 <table>
+    <!--
          <tr>
              <th v-for="(v, k, i) in tabledata[0]">{{ k }}</th>
         </tr>
         <tr v-for="x in tabledata">
             <td v-for="z in x">{{ z }}</td>
-        </tr>
+        </tr>-->
     </table>
+    <div v-for="item in filteredData">{{item}}</div>
 </div>
     
     
@@ -28,7 +30,7 @@
 import Vue from "vue";
 import Filters from "../Filters.vue";
 import {getColor, iterateDataValues} from "../utils";
-import {ChoroplethIndicatorMetadata, FilterOption} from "../../../generated";
+import {ChoroplethIndicatorMetadata, FilterOption, NestedFilterOption} from "../../../generated";
 import {Dict, Filter, IndicatorValuesDict, NumericRange} from "../../../types";
 import {
         ChoroplethSelections,
@@ -42,9 +44,19 @@ import {flattenOptions, flattenToIdSet} from "../../../utils";
 interface Props {
         tabledata: any[],
         indicators: ChoroplethIndicatorMetadata[],
-        selections: ChoroplethSelections
-        // filters: Filter[],
-    }
+        selections: ChoroplethSelections,
+        filters: Filter[],
+        areaFilterId: string
+}
+
+interface Computed {
+    nonAreaFilters: Filter[],
+    areaFilter: Filter,
+    filteredData: any[],
+    flattenedAreas: Dict<NestedFilterOption>,
+    selectedAreaIds: string[],
+    selectedAreaFilterOptions: FilterOption[],
+}
 
 const props = {
     tabledata: {
@@ -68,7 +80,7 @@ const props = {
 }
 
 // export default Vue.extend<Props>({
-export default Vue.extend<{}, {}, {}, Props>({
+export default Vue.extend<{}, {}, Computed, Props>({
     name: "table-view",
     props: props,
     mounted() {
@@ -81,48 +93,41 @@ export default Vue.extend<{}, {}, {}, Props>({
       }
     },
     computed: {
-        // nonAreaFilters() {
-        //         return this.filters.filter((f: Filter) => f.id != this.areaFilterId);
-        //     },
+        nonAreaFilters() {
+             return this.filters.filter((f: Filter) => f.id != this.areaFilterId);
+         },
         // colorIndicator(): ChoroplethIndicatorMetadata {
         //         return this.indicators.find(i => i.indicator == this.selections.indicatorId)!!;
         //     },
-        // areaFilter() {
-        //         return this.filters.find((f: Filter) => f.id == this.areaFilterId)!!;
-        //     },
-        // selectedAreaFilterOptions() {
-        //         const selectedOptions = this.selections.selectedFilterOptions[this.areaFilterId];
-        //         if (selectedOptions && selectedOptions.length > 0) {
-        //             return selectedOptions
-        //         }
-        //         return this.areaFilter ? this.areaFilter.options : []; //consider all top level areas to be selected if none are
-        //     },
-        // flattenedAreas() {
-        //         return this.areaFilter ? flattenOptions(this.areaFilter.options) : {};
-        //     },
-        // selectedAreaIds() {
-        //         const selectedAreaIdSet = flattenToIdSet(this.selectedAreaFilterOptions.map(o => o.id), this.flattenedAreas);
-
-        //         //Should also ensure include top level (country) included if no filters selected
-        //         const selectedOptions = this.selections.selectedFilterOptions[this.areaFilterId];
-        //         if (!selectedOptions || selectedOptions.length == 0) {
-        //             this.currentLevelFeatureIds.forEach(id => selectedAreaIdSet.add(id));
-        //         }
-
-        //         return Array.from(selectedAreaIdSet);
-        //     },
-
-
-        filterData(indicatorMeta: ChoroplethIndicatorMetadata, selectedFilterValues: Dict<FilterOption[]>) {
-            const result = {}
-            iterateDataValues(this.tabledata, this.colorIndicator, this.selectedAreaIds, this.nonAreaFilters, this.selections.selectedFilterOptions,
-        (areaId: string, indicatorMeta: ChoroplethIndicatorMetadata, value: number) => {
-            // result[areaId] = {
-            //     value: value
-            // }
-        })
-        console.log('filterdata', result)
-        return result
+        areaFilter() {
+                 return this.filters.find((f: Filter) => f.id == this.areaFilterId)!!;
+             },
+        flattenedAreas() {
+                 return this.areaFilter ? flattenOptions(this.areaFilter.options) : {};
+             },
+        selectedAreaIds() {
+            const selectedAreaIdSet = flattenToIdSet(this.selectedAreaFilterOptions.map(o => o.id), this.flattenedAreas);
+            return Array.from(selectedAreaIdSet);
+        },
+        selectedAreaFilterOptions() {
+            const selectedOptions = this.selections.selectedFilterOptions[this.areaFilterId];
+            if (selectedOptions && selectedOptions.length > 0) {
+                return selectedOptions
+            }
+            return this.areaFilter ? this.areaFilter.options : []; //consider all top level areas to be selected if none are
+        },
+        filteredData() {
+            const result: any[] = [];
+            iterateDataValues(this.tabledata,
+                this.indicators,
+                this.selectedAreaIds,
+                this.nonAreaFilters,
+                this.selections.selectedFilterOptions,
+                (areaId: string, indicatorMeta: ChoroplethIndicatorMetadata, value: number) => {
+                    result.push({areaId, indicatorMeta, value});
+                });
+            console.log('filterdata', result)
+            return result;
         }
 
     //     filterData2(){
