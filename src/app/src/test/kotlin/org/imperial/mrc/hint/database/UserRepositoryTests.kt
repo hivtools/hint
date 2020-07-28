d1f25da0600730dad61b73142675549063bc2510
Alex <alex.hill@gmail.com>
2020-07-28 14:49:22 +0100

    update methods to work with bytearrays

--- src/app/src/test/kotlin/org/imperial/mrc/hint/database/UserRepositoryTests.kt
+++ src/app/src/test/kotlin/org/imperial/mrc/hint/database/UserRepositoryTests.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.imperial.mrc.hint.db.Tables.ADR_KEY
 import org.imperial.mrc.hint.db.UserRepository
 import org.imperial.mrc.hint.logic.UserLogic
+import org.imperial.mrc.hint.security.Encryption
 import org.jooq.DSLContext
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -30,42 +31,40 @@ class UserRepositoryTests {
     private lateinit var userRepo: UserLogic
 
     private val testEmail = "test@test.com"
+    private val encryption = Encryption()
+    private val testKey = "123-some-key"
 
     @Test
     fun `can save new API key`() {
         userRepo.addUser(testEmail, "pw")
 
-        sut.saveADRKey(testEmail, "encryptedkey")
+        val encryptedKey = encryption.encrypt(testKey)
+        sut.saveADRKey(testEmail, encryptedKey)
 
-        val key = dsl.select(ADR_KEY.API_KEY)
-                .from(ADR_KEY)
-                .where(ADR_KEY.USER_ID.eq(testEmail))
-                .fetchAnyInto(String::class.java)
-
-        assertThat(key).isEqualTo("encryptedkey")
+        val key = sut.getADRKey(testEmail)
+        assertThat(key).isEqualTo(encryptedKey)
     }
 
     @Test
     fun `can update API key`() {
         userRepo.addUser(testEmail, "pw")
+        val encryptedKey = encryption.encrypt(testKey)
+        val newEncryptedKey = encryption.encrypt("new-key-456")
 
-        sut.saveADRKey(testEmail, "encryptedkey")
-        sut.saveADRKey(testEmail, "newkey")
+        sut.saveADRKey(testEmail, encryptedKey)
+        sut.saveADRKey(testEmail, newEncryptedKey)
 
-        val key = dsl.select(ADR_KEY.API_KEY)
-                .from(ADR_KEY)
-                .where(ADR_KEY.USER_ID.eq(testEmail))
-                .fetchAnyInto(String::class.java)   
-
-        assertThat(key).isEqualTo("newkey")
+        val key = sut.getADRKey(testEmail)
+        assertThat(key).isEqualTo(newEncryptedKey)
     }
 
 
     @Test
     fun `can delete API key`() {
         userRepo.addUser(testEmail, "pw")
+        val encryptedKey = encryption.encrypt(testKey)
 
-        sut.saveADRKey(testEmail, "encryptedkey")
+        sut.saveADRKey(testEmail, encryptedKey)
         sut.deleteADRKey(testEmail)
 
         val keys = dsl.selectFrom(ADR_KEY)
@@ -73,5 +72,22 @@ class UserRepositoryTests {
                 .fetch()
 
         assertThat(keys.any()).isFalse()
+    }
+
+    @Test
+    fun `can retrieve API key`() {
+        userRepo.addUser(testEmail, "pw")
+        val encryptedKey = encryption.encrypt(testKey)
+
+        sut.saveADRKey(testEmail, encryptedKey)
+        val result = sut.getADRKey(testEmail)!!
+        assertThat(encryption.decrypt(result)).isEqualTo(testKey)
+    }
+
+    @Test
+    fun `returns null if API key does not exist`() {
+        userRepo.addUser(testEmail, "pw")
+        val result = sut.getADRKey(testEmail)
+        assertThat(result).isEqualTo(null)
     }
 }
