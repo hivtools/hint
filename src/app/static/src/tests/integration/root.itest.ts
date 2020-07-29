901877d00f6a0f811eb4d5641dc49d34edd5454c
Alex <alex.hill@gmail.com>
2020-07-29 18:05:22 +0100

    fix integration tests

--- src/app/static/src/tests/integration/root.itest.ts
+++ src/app/static/src/tests/integration/root.itest.ts
@@ -2,7 +2,7 @@ import {actions} from "../../app/store/root/actions";
 import {login, rootState} from "./integrationTest";
 import {RootMutation} from "../../app/store/root/mutations";
 
-describe("Baseline actions", () => {
+describe("Root actions", () => {
 
     beforeAll(async () => {
         await login();
@@ -12,23 +12,23 @@ describe("Baseline actions", () => {
         const commit = jest.fn();
         await actions.fetchADRKey({commit, rootState} as any);
 
-        expect(commit.mock.calls[1][0]["type"]).toBe(RootMutation.UpdateADRKey);
-        expect(commit.mock.calls[1][0]["payload"]).toBe(null);
+        expect(commit.mock.calls[0][0]["type"]).toBe(RootMutation.UpdateADRKey);
+        expect(commit.mock.calls[0][0]["payload"]).toBe(null);
     });
 
     it("can save ADR key", async () => {
         const commit = jest.fn();
         await actions.saveADRKey({commit, rootState} as any, "1234");
 
-        expect(commit.mock.calls[1][0]["type"]).toBe(RootMutation.UpdateADRKey);
-        expect(commit.mock.calls[1][0]["payload"]).toBe("1234");
+        expect(commit.mock.calls[0][0]["type"]).toBe(RootMutation.UpdateADRKey);
+        expect(commit.mock.calls[0][0]["payload"]).toBe("1234");
     });
 
     it("can delete ADR key", async () => {
         const commit = jest.fn();
         await actions.deleteADRKey({commit, rootState} as any);
 
-        expect(commit.mock.calls[1][0]["type"]).toBe(RootMutation.UpdateADRKey);
-        expect(commit.mock.calls[1][0]["payload"]).toBe(null);
+        expect(commit.mock.calls[0][0]["type"]).toBe(RootMutation.UpdateADRKey);
+        expect(commit.mock.calls[0][0]["payload"]).toBe(null);
     });
 });
