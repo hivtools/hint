8f76f177cf1417dffb2abd3c53dfdfbaeedb5539
Alex <alex.hill@gmail.com>
2020-09-02 14:17:39 +0100

    check whether resources are out of date

--- src/app/static/src/tests/components/adr/selectDataset.test.ts
+++ src/app/static/src/tests/components/adr/selectDataset.test.ts
@@ -4,7 +4,7 @@ import {mount, shallowMount} from "@vue/test-utils";
 import SelectDataset from "../../../app/components/adr/SelectDataset.vue";
 import Modal from "../../../app/components/Modal.vue";
 import TreeSelect from '@riophae/vue-treeselect'
-import {mockBaselineState, mockDatasetResource, mockRootState} from "../../mocks";
+import {mockBaselineState, mockDataset, mockDatasetResource, mockRootState, mockShapeResponse} from "../../mocks";
 import {BaselineState} from "../../../app/store/baseline/baseline";
 import LoadingSpinner from "../../../app/components/LoadingSpinner.vue";
 import {BaselineMutation} from "../../../app/store/baseline/mutations";
@@ -12,6 +12,7 @@ import {BaselineActions} from "../../../app/store/baseline/actions";
 import {SurveyAndProgramActions} from "../../../app/store/surveyAndProgram/actions";
 import {ADRSchemas} from "../../../app/types";
 import {RootState} from "../../../app/root";
+import {InfoIcon} from "vue-feather-icons";
 import Mock = jest.Mock;
 
 describe("select dataset", () => {
@@ -27,7 +28,7 @@ describe("select dataset", () => {
     }
 
     const pjnz = {resource_type: schemas.pjnz, url: "pjnz.pjnz", revision_id: "pj1234"}
-    const shape = {resource_type: schemas.shape, url: "shape.geojson",revision_id: "sh1234"}
+    const shape = {resource_type: schemas.shape, url: "shape.geojson", revision_id: "sh1234"}
     const pop = {resource_type: schemas.population, url: "pop.csv", revision_id: "po1234"}
     const survey = {resource_type: schemas.survey, url: "survey.csv", revision_id: "su1234"}
     const program = {resource_type: schemas.programme, url: "program.csv", revision_id: "pr1234"}
@@ -58,11 +59,13 @@ describe("select dataset", () => {
     }
 
     const setDatasetMock = jest.fn();
+    const markResourcesUpdatedMock = jest.fn();
 
     const baselineActions: Partial<BaselineActions> & ActionTree<any, any> = {
         importShape: jest.fn(),
         importPopulation: jest.fn(),
-        importPJNZ: jest.fn()
+        importPJNZ: jest.fn(),
+        refreshDatasetMetadata: jest.fn()
     }
 
     const surveyProgramActions: Partial<SurveyAndProgramActions> & ActionTree<any, any> = {
@@ -97,7 +100,12 @@ describe("select dataset", () => {
 
     afterEach(() => {
         jest.resetAllMocks();
-    })
+    });
+
+    it("refreshes selected dataset metdata on mount", () => {
+        const rendered = shallowMount(SelectDataset, {store: getStore()});
+        expect((baselineActions.refreshDatasetMetadata as Mock).mock.calls.length).toBe(1);
+    });
 
     it("renders select dataset button when no dataset is selected", () => {
         const rendered = shallowMount(SelectDataset, {store: getStore()});
@@ -111,6 +119,57 @@ describe("select dataset", () => {
             })
         });
         expect(rendered.find("button").text()).toBe("Edit");
+    });
+
+    it("does not render refresh button or info icon when no resources are out of date", () => {
+        const rendered = shallowMount(SelectDataset, {
+            store: getStore({
+                selectedDataset: fakeDataset
+            })
+        });
+        expect(rendered.findAll("button").length).toBe(1);
+        expect(rendered.findAll(InfoIcon).length).toBe(0);
+    });
+
+    it("shows refresh button and info icon if any resource is out of date", () => {
+        const fakeDataset = mockDataset();
+        fakeDataset.resources.pjnz = mockDatasetResource({outOfDate: true});
+        const store = getStore({selectedDataset: fakeDataset});
+        const mockTooltipDirective = jest.fn();
+        const rendered = mount(SelectDataset, {
+            store,
+            directives: {"tooltip": mockTooltipDirective},
+            stubs: ["tree-select"]
+        });
+        const buttons = rendered.findAll("button");
+        expect(buttons.at(0).text()).toBe("Refresh");
+        expect(buttons.at(1).text()).toBe("Edit");
+
+        expect(rendered.findAll(InfoIcon).length).toBe(1);
+
+        expect(mockTooltipDirective.mock.calls[0][0].innerHTML)
+            .toContain("<circle cx=\"12\" cy=\"12\" r=\"10\"></circle>"); // tooltip should be on the icon
+        expect(mockTooltipDirective.mock.calls[0][1].value)
+            .toBe("This dataset has been updated in the ADR. Use the refresh button to import the latest files.");
+    });
+
+    it("renders modal and spinner while refreshing", async (done) => {
+        const fakeDataset = mockDataset();
+        fakeDataset.resources.pjnz = mockDatasetResource({outOfDate: true, url: "pjnz.url"})
+        const store = getStore({selectedDataset: fakeDataset});
+        const rendered = shallowMount(SelectDataset, {store, sync: false});
+        rendered.findAll("button").at(0).trigger("click");
+
+        await Vue.nextTick();
+
+        expect(rendered.find(Modal).props("open")).toBe(true);
+        expect(rendered.findAll(LoadingSpinner).length).toBe(1);
+
+        setTimeout(() => {
+            expect(rendered.find(Modal).props("open")).toBe(false);
+            expect(rendered.findAll(LoadingSpinner).length).toBe(0);
+            done();
+        }, 200);
     });
 
     it("renders selected dataset if it exists", () => {
@@ -175,18 +234,24 @@ describe("select dataset", () => {
         await Vue.nextTick();
 
         expect(rendered.findAll(TreeSelect).length).toBe(1);
+        expect(rendered.find(Modal).findAll("button").length).toBe(2);
+        expect(rendered.findAll("p").length).toBe(0);
+        expect(rendered.find("h4").text()).toBe("Browse ADR");
+
         rendered.setData({newDatasetId: "id1"});
         rendered.find(Modal).find("button").trigger("click");
 
         await Vue.nextTick();
         expect(setDatasetMock.mock.calls[0][1]).toEqual(fakeDataset);
 
-        // loading spinner should render and buttons temporarily disabled
+        // loading spinner should render and buttons hidden
         const buttons = rendered.find(Modal).findAll("button");
         expect(rendered.findAll(TreeSelect).length).toBe(0);
         expect(rendered.findAll(LoadingSpinner).length).toBe(1);
-        expect(buttons.at(0).attributes("disabled")).toBe("disabled");
-        expect(buttons.at(1).attributes("disabled")).toBe("disabled");
+        expect(buttons.length).toBe(0);
+        expect(rendered.find("p").text())
+            .toBe("Importing files - this may take several minutes. Please do not close your browser.");
+        expect(rendered.findAll("h4").length).toBe(0);
 
         await Vue.nextTick();
         await Vue.nextTick();
@@ -343,6 +408,41 @@ describe("select dataset", () => {
         await Vue.nextTick();
         // await just one tick for baseline actions to return, survey actions will not fire
         // because shape is not present
+
+        expect(rendered.findAll(LoadingSpinner).length).toBe(0);
+        expect(rendered.find(Modal).props("open")).toBe(false);
+    });
+
+    it("imports survey and program files if pre-existing shape file is present", async () => {
+        const store = getStore(
+            {
+                shape: mockShapeResponse()
+            },
+            {
+                adrDatasets: [{...fakeRawDatasets[0], resources: [survey, program, anc]}]
+            });
+
+        const rendered = mount(SelectDataset, {store, sync: false, stubs: ["tree-select"]});
+        rendered.find("button").trigger("click");
+
+        await Vue.nextTick();
+
+        expect(rendered.findAll(TreeSelect).length).toBe(1);
+        rendered.setData({newDatasetId: "id1"});
+        rendered.find(Modal).find("button").trigger("click");
+
+        await Vue.nextTick();
+
+        expect(rendered.findAll(LoadingSpinner).length).toBe(1);
+
+        await Vue.nextTick();
+
+        expect((surveyProgramActions.importSurvey as Mock).mock.calls[0][1]).toBe("survey.csv");
+        expect((surveyProgramActions.importProgram as Mock).mock.calls[0][1]).toBe("program.csv");
+        expect((surveyProgramActions.importANC as Mock).mock.calls[0][1]).toBe("anc.csv");
+
+        await Vue.nextTick(); // once for baseline actions to return
+        await Vue.nextTick(); // once for survey actions to return
 
         expect(rendered.findAll(LoadingSpinner).length).toBe(0);
         expect(rendered.find(Modal).props("open")).toBe(false);
