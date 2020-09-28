07d1e1184ad2beb5fbdc61ac23baa79c1947c070
Alex <alex.hill@gmail.com>
2020-09-28 12:27:43 +0100

    tests for ShareProject.vue

--- src/app/static/src/tests/components/projects/shareProject.test.ts
+++ src/app/static/src/tests/components/projects/shareProject.test.ts
@@ -4,6 +4,7 @@ import Modal from "../../../app/components/Modal.vue";
 import Vuex from "vuex";
 import {emptyState} from "../../../app/root";
 import registerTranslations from "../../../app/store/translations/registerTranslations";
+import LoadingSpinner from "../../../app/components/LoadingSpinner.vue";
 
 describe("ShareProject", () => {
 
@@ -45,8 +46,8 @@ describe("ShareProject", () => {
         expect(wrapper.find(Modal).props("open")).toBe(false);
     });
 
-    it("if emails are invalid, valiation feedback is shown and no action taken", () => {
-        const wrapper = shallowMount(ShareProject, {
+    it("if emails are invalid, validation feedback is shown and no action taken", () => {
+        const wrapper = mount(ShareProject, {
             propsData: {
                 project: {id: 1, name: "p1"}
             },
@@ -55,6 +56,33 @@ describe("ShareProject", () => {
 
         const link = wrapper.find("a");
         link.trigger("click");
+        wrapper.find(Modal).find("input").setValue("bademail");
+        wrapper.find(Modal).findAll("button").at(0).trigger("click");
+        expect(wrapper.find(Modal).props("open")).toBe(true);
+        expect(wrapper.find(Modal).findAll(LoadingSpinner).length).toBe(0);
+        expect(wrapper.find(Modal).find("input").classes()).toContain("is-invalid");
+    });
+
+    it("if emails are valid, project is shared", (done) => {
+        const wrapper = mount(ShareProject, {
+            propsData: {
+                project: {id: 1, name: "p1"}
+            },
+            store: createStore()
+        });
+
+        const link = wrapper.find("a");
+        link.trigger("click");
+        wrapper.find(Modal).find("input").setValue("goodemail@gmail.com");
+        wrapper.find(Modal).findAll("button").at(0).trigger("click");
+        expect(wrapper.find(Modal).props("open")).toBe(true);
+        expect(wrapper.find(Modal).findAll(LoadingSpinner).length).toBe(1);
+
+        setTimeout(() => {
+            expect(wrapper.find(Modal).props("open")).toBe(false);
+            expect(wrapper.find(Modal).findAll(LoadingSpinner).length).toBe(0);
+            done();
+        }, 200)
     });
 
 });
