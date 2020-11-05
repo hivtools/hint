bdb1c68ad6c0533b90b196025be46197b9af16aa
Alex <alex.hill@gmail.com>
2020-11-05 12:50:46 +0000

    fix build-app script

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -8,7 +8,7 @@ export NODE_ENV=production
 
 $HERE/../src/gradlew -p $HERE/../src :app:bootDistTar
 
-( cd src ;
+( cd $HERE/../src ;
 docker build --build-arg SPRING_PROFILES_ACTIVE=$1 -f ../Dockerfile --tag $APP_DOCKER_COMMIT_TAG . \
     && docker tag $APP_DOCKER_COMMIT_TAG $APP_DOCKER_BRANCH_TAG
 )
