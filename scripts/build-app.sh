de47222ca878c22002b9dfd3b70818286d54ddc7
Alex <alex.hill@gmail.com>
2020-11-05 12:46:12 +0000

    add travis scripts back

---
+++ scripts/build-app.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -ex
+
+HERE=$(realpath  "$(dirname $0)")
+. $HERE/common
+
+export NODE_ENV=production
+
+$HERE/../src/gradlew -p $HERE/../src :app:bootDistTar
+
+( cd src ;
+docker build --build-arg SPRING_PROFILES_ACTIVE=$1 -f ../Dockerfile --tag $APP_DOCKER_COMMIT_TAG . \
+    && docker tag $APP_DOCKER_COMMIT_TAG $APP_DOCKER_BRANCH_TAG
+)
