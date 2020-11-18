4d9b281665f7c08b9077ef85f0b1a0c907ebb1cd
Lekan Anifowoshe <anney.lake@gmail.com>
2020-11-18 17:09:47 +0000

    add test

--- src/app/static/src/tests/components/projects/projectHistory.test.ts
+++ src/app/static/src/tests/components/projects/projectHistory.test.ts
@@ -280,7 +280,8 @@ describe("Project history component", () => {
     it("hides modal and does not invoke action when click cancel", async () => {
 
         const wrapper = getWrapper(testProjects);
-        const deleteLink = wrapper.find("#v-s11").findAll(".version-cell").at(5).find("button");
+        const deleteLink = wrapper.find("#v-s11").findAll(".version-cell").at(4).find("button");
+        expect(deleteLink.element).toBe("test")
         deleteLink.trigger("click");
         await Vue.nextTick();
 
