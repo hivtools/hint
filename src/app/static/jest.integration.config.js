188089fd257cd5638092bf293b0c14eb9e303e3a
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-11-27 15:09:51 +0000

    Set a separate (long) timeout for front end ADR integration tests

--- src/app/static/jest.integration.config.js
+++ src/app/static/jest.integration.config.js
@@ -3,5 +3,5 @@ var config = require('./jest.config');
 config.testRegex = "(/__tests__/.*|(\\.|/)(itest))\\.[jt]sx?$";
 config.globals = {...config.globals, "appUrl": "http://localhost:8080/"};
 config.coverageDirectory = "./coverage/integration/";
-config.testTimeout = 20000;
+config.testTimeout = 6000;
 module.exports = config;
