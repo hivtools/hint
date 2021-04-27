1787123119c382ea2765943792e6814df51354ee
Nicolas Dolan <nicolas.dolan@ymail.com>
2021-04-27 12:14:31 +0100

    cleaned up code

--- src/app/static/src/app/store/adr/actions.ts
+++ src/app/static/src/app/store/adr/actions.ts
@@ -75,9 +75,6 @@ export const actions: ActionTree<ADRState, RootState> & ADRActions = {
                 //We may also have to fetch the selected dataset metadata too, if not loaded during this session
                 await getAndSetDatasets(context, selectedDataset.id)
                 selectedDatasetOrgId = rootState.baseline.selectedDataset!.organization.id
-                // selectedDatasetOrgId = (baseline as BaselineState).selectedDataset!.organization.id
-                // selectedDatasetOrgId = rootState.baseline.selectedDataset!.organization.id;
-                // selectedDatasetOrgId = selectedDataset.organization!.id;
             } else {
                 selectedDatasetOrgId = selectedDataset.organization.id;
             }
