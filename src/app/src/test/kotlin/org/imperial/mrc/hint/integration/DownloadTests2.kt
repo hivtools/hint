7c15b4618b128d07609cd6e0004c27709d23cb0b
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2021-04-28 08:33:01 +0100

    Split tests

---
+++ src/app/src/test/kotlin/org/imperial/mrc/hint/integration/DownloadTests2.kt
@@ -0,0 +1,66 @@
+package org.imperial.mrc.hint.integration
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.boot.test.web.client.postForEntity
+import org.springframework.http.ResponseEntity
+
+class DownloadTests2 : SecureIntegrationTests()
+{
+
+    @BeforeEach
+    fun setup()
+    {
+        authorize()
+        testRestTemplate.getForEntity<String>("/")
+    }
+
+    @Disabled
+    @Test
+    fun `can download Spectrum results`()
+    {
+        val id = waitForModelRunResult()
+        val responseEntity = testRestTemplate.getForEntity<ByteArray>("/download/spectrum/$id")
+        assertSuccess(responseEntity)
+        assertResponseHasExpectedDownloadHeaders(responseEntity)
+
+    }
+
+    @Test
+    fun `can download summary data`()
+    {
+        val id = waitForModelRunResult()
+        val responseEntity = testRestTemplate.getForEntity<ByteArray>("/download/summary/$id")
+        assertSuccess(responseEntity)
+        assertResponseHasExpectedDownloadHeaders(responseEntity)
+    }
+
+    @Disabled
+    @Test
+    fun `can download coarse output results`()
+    {
+        val id = waitForModelRunResult()
+        val responseEntity = testRestTemplate.getForEntity<ByteArray>("/download/coarse-output/$id")
+        assertSuccess(responseEntity)
+        assertResponseHasExpectedDownloadHeaders(responseEntity)
+
+    }
+
+    fun assertResponseHasExpectedDownloadHeaders(response: ResponseEntity<ByteArray>)
+    {
+        val headers = response.headers
+        assertThat(headers["Content-Type"]?.first()).isEqualTo("application/octet-stream")
+
+        val contentLength = headers["Content-Length"]?.first()!!.toInt()
+        assertThat(contentLength).isGreaterThan(0)
+        val bodyLength = response.body?.count()
+        assertThat(contentLength).isEqualTo(bodyLength)
+        assertThat(headers["Connection"]?.first()).isNotEqualTo("keep-alive")
+    }
+}
