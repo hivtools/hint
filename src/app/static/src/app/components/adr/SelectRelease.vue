42fb925a0aa2f2d23e4f7f1398ffa6b939aa23b7
Nicolas Dolan <nicolas.dolan@ymail.com>
2021-07-22 15:14:56 +0100

    saving changes

--- src/app/static/src/app/components/adr/SelectRelease.vue
+++ src/app/static/src/app/components/adr/SelectRelease.vue
@@ -1,14 +1,57 @@
 <template>
-    <div id="selectRelease"></div>
+    <div id="selectRelease">
+        <tree-select
+            :multiple="false"
+            :searchable="true"
+            :options="datasetOptions"
+            :placeholder="select"
+            v-model="newReleaseId"
+        >
+            <label
+                slot="option-label"
+                slot-scope="{ node }"
+                v-html="node.raw.customLabel"
+            >
+            </label>
+        </tree-select>
+    </div>
 </template>
 
 <script lang="ts">
     import Vue from "vue";
-    import { mapActionByName } from "../../utils";
+    import { mapActionByName, mapStateProp } from "../../utils";
     import TreeSelect from "@riophae/vue-treeselect";
+    import { ADRState } from "../../store/adr/adr";
+
+    const namespace = "adr";
 
     export default Vue.extend({
+        components: {
+            TreeSelect,
+        },
         props: ["selectedDataset"],
+        data() {
+            return {
+                newReleaseId: null,
+            };
+        },
+        computed: {
+            releases: mapStateProp<ADRState, any[]>(
+                namespace,
+                (state: ADRState) => state.releases
+            ),
+            releaseOptions() {
+                return this.datasets.map((d) => ({
+                    id: d.id,
+                    label: d.title,
+                    customLabel: `${d.title}
+                            <div class="text-muted small" style="margin-top:-5px; line-height: 0.8rem">
+                                (${d.name})<br/>
+                                <span class="font-weight-bold">${d.organization.title}</span>
+                            </div>`,
+                }));
+            },
+        },
         methods: {
             getReleases: mapActionByName("adr", "getReleases"),
         },
@@ -16,6 +59,6 @@
             selectedDataset(id) {
                 this.getReleases(id);
             },
-        }
+        },
     });
 </script>
