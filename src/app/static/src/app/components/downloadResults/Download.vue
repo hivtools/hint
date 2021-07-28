e7780c8b3c16ef0dfb091b2378bd2d0dea8427d1
Lekan Anifowoshe <anney.lake@gmail.com>
2021-07-28 14:25:05 +0100

    fix indentation

--- src/app/static/src/app/components/downloadResults/Download.vue
+++ src/app/static/src/app/components/downloadResults/Download.vue
@@ -2,6 +2,7 @@
   <div id="download">
     <h4 v-translate="translateKey.header"></h4>
     <button class="btn btn-red btn-lg my-3"
+            :disabled="file.downloading"
             @click="download">
       <span v-translate="translateKey.button"></span>
       <download-icon size="20" class="icon ml-2" style="margin-top: -4px;"></download-icon>
@@ -16,52 +17,52 @@
 </template>
 
 <script lang="ts">
-  import Vue from "vue";
-  import DownloadProgress from "./DownloadProgress.vue";
-  import ErrorAlert from "../ErrorAlert.vue";
-  import {DownloadIcon} from "vue-feather-icons";
-  import {DownloadResultsDependency} from "../../types";
+    import Vue from "vue";
+    import DownloadProgress from "./DownloadProgress.vue";
+    import ErrorAlert from "../ErrorAlert.vue";
+    import {DownloadIcon} from "vue-feather-icons";
+    import {DownloadResultsDependency} from "../../types";
 
-  interface downloadTranslate {
-    header: string,
-    button: string
-  }
-
-  interface Props {
-    file: DownloadResultsDependency,
-    translateKey: downloadTranslate,
-    modalOpen: boolean
-  }
-
-  interface Methods {
-    download: () => void
-  }
-
-  export default Vue.extend<unknown, Methods, unknown, Props>({
-    name: "Download",
-    components: {
-      DownloadIcon,
-      ErrorAlert,
-      DownloadProgress
-    },
-    props: {
-      file: {
-        required: true,
-        type: Object
-      },
-      translateKey: {
-        required: true,
-        type: Object
-      },
-      modalOpen: {
-        required: true,
-        type: Boolean
-      }
-    },
-    methods: {
-      download() {
-        this.$emit("click")
-      }
+    interface downloadTranslate {
+      header: string,
+      button: string
     }
-  })
+
+    interface Props {
+      file: DownloadResultsDependency,
+      translateKey: downloadTranslate,
+      modalOpen: boolean
+    }
+
+    interface Methods {
+      download: () => void
+    }
+
+    export default Vue.extend<unknown, Methods, unknown, Props>({
+      name: "Download",
+      components: {
+        DownloadIcon,
+        ErrorAlert,
+        DownloadProgress
+      },
+      props: {
+        file: {
+          required: true,
+          type: Object
+        },
+        translateKey: {
+          required: true,
+          type: Object
+        },
+        modalOpen: {
+          required: true,
+          type: Boolean
+        }
+      },
+      methods: {
+        download() {
+          this.$emit("click")
+        }
+      }
+    })
 </script>
