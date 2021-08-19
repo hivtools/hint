ac5254496dc166304a74f499b7958de679d6f9ed
Emma Russell <EmmaRussellKilburn@gmail.com>
2021-08-19 15:48:11 +0100

    Added Time series tab to SAP, and GenericChart component which receives metadata

---
+++ src/app/static/src/app/components/genericChart/GenericChart.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        {{JSON.stringify(chartMetadata)}}
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+    import {GenericChartMetadata, GenericChartMetadataResponse} from "../../types";
+
+    interface Props {
+        metadata: GenericChartMetadataResponse
+        chartId: string
+    }
+
+    interface Computed {
+        chartMetadata: GenericChartMetadata
+    }
+
+    export default Vue.extend<unknown, unknown, Computed, Props>({
+        name: "GenericChart",
+        props: {
+            metadata: Object,
+            chartId: String
+        },
+        computed: {
+            chartMetadata() {
+                return this.metadata[this.chartId]!;
+            }
+        }
+    });
+</script>
+
+<style scoped>
+
+</style>
