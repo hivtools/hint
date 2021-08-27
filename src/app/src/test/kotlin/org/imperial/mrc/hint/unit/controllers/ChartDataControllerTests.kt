0b719e9ccd548404864f1591183ab7865f99b583
Emma Russell <EmmaRussellKilburn@gmail.com>
2021-08-27 12:56:54 +0100

    Backend changes for PR comments

--- src/app/src/test/kotlin/org/imperial/mrc/hint/unit/controllers/ChartDataControllerTests.kt
+++ src/app/src/test/kotlin/org/imperial/mrc/hint/unit/controllers/ChartDataControllerTests.kt
@@ -7,8 +7,11 @@ import org.imperial.mrc.hint.FileManager
 import org.imperial.mrc.hint.FileType
 import org.imperial.mrc.hint.clients.HintrAPIClient
 import org.imperial.mrc.hint.controllers.ChartDataController
+import org.imperial.mrc.hint.exceptions.HintException
 import org.imperial.mrc.hint.models.VersionFileWithPath
+import org.imperial.mrc.hint.helpers.TranslationAssert
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
 class ChartDataControllerTests {
@@ -44,5 +47,15 @@ class ChartDataControllerTests {
         val sut = ChartDataController(mockFileManager, mockClient)
         val response = sut.inputTimeSeries("programme")
         assertThat(response).isSameAs(mockResponse)
+    }
+
+    @Test
+    fun `throws exception when input time series type is unknown`()
+    {
+        val sut = ChartDataController(mock(), mock())
+        TranslationAssert.assertThatThrownBy{ sut.inputTimeSeries("not-a-real-type") }
+                .isInstanceOf(HintException::class.java)
+                .matches { (it as HintException).httpStatus == HttpStatus.BAD_REQUEST }
+                .hasTranslatedMessage("Unknown input time series type.")
     }
 }
