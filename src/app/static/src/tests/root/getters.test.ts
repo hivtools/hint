a8b3a11aab949678f9f59b7618cd4aa1086dc7a3
Lekan Anifowoshe <anney.lake@gmail.com>
2021-10-18 12:49:20 +0100

    add more tests

--- src/app/static/src/tests/root/getters.test.ts
+++ src/app/static/src/tests/root/getters.test.ts
@@ -1,46 +1,78 @@
 import {mockModelCalibrateState, mockModelOptionsState, mockModelRunState, mockRootState} from "../mocks";
 import {getters} from "../../app/store/root/getters";
 import {Warning} from "../../app/generated";
-import {STEPS} from "../../app/root";
+import {Steps} from "../../app/root";
 
 describe(`root getters`, () => {
 
-    const warnings: Warning[] = [
-        {text: "model option test", locations: ["model_options"]},
-        {text: "model calibrate test", locations: ["model_calibrate"]},
+    const modelOptionWarnings: Warning[] = [
+        {text: "model option test", locations: ["model_options"]}
+    ]
+
+    const calibrateWarnings: Warning[] = [
+        {text: "model calibrate test", locations: ["model_options", "model_calibrate"]}
+    ]
+
+    const modelRunWarnings: Warning[] = [
         {text: "model run test", locations: ["model_fit"]}
     ]
 
     const testState = () => {
         return mockRootState({
-            modelOptions: mockModelOptionsState({warnings: warnings}),
-            modelRun: mockModelRunState({warnings: warnings}),
-            modelCalibrate: mockModelCalibrateState({warnings: warnings})
+            modelOptions: mockModelOptionsState({warnings: modelOptionWarnings}),
+            modelRun: mockModelRunState({warnings: modelRunWarnings}),
+            modelCalibrate: mockModelCalibrateState({warnings: calibrateWarnings})
         })
     }
 
-    it(`can get model run warnings`, () => {
+    it(`can get modelRun warnings when on model run step`, () => {
         const rootState = testState()
 
         const warn = getters.warnings(rootState, null, testState() as any, null)
-        const result = warn(STEPS.modelRun).modelRun
-        expect(result).toEqual([warnings[2]])
+        const result = warn(Steps.modelRun).modelRun
+        expect(result).toEqual(modelRunWarnings)
     })
 
-    it(`can get model calibrate warnings`, () => {
+    it(`can get modelCalibrate warnings when on model calibrate step`, () => {
         const rootState = testState()
 
         const warn = getters.warnings(rootState, null, testState() as any, null)
-        const result = warn(STEPS.modelCalibrate).modelCalibrate
-        expect(result).toEqual([warnings[1]])
+        const result = warn(Steps.modelCalibrate).modelCalibrate
+        expect(result).toEqual([
+            {
+                "locations": ["model_options", "model_calibrate"],
+                "text": "model calibrate test"
+            }
+        ])
+    })
+
+    it(`can get modelCalibrate warnings if exists in a modelOptions step`, () => {
+        const rootState = testState()
+
+        const warn = getters.warnings(rootState, null, testState() as any, null)
+        const result = warn(Steps.modelOptions).modelCalibrate
+        expect(result).toEqual([
+            {
+                locations: ["model_options", "model_calibrate"],
+                text: "model calibrate test"
+            }
+        ])
+    })
+
+    it(`does not get modelOptions warnings if it does not exist on modelCalibrate step`, () => {
+        const rootState = testState()
+
+        const warn = getters.warnings(rootState, null, testState() as any, null)
+        const result = warn(Steps.modelCalibrate).modelOptions
+        expect(result).toEqual([])
     })
 
     it(`can get model options warnings`, () => {
         const rootState = testState()
 
         const warn = getters.warnings(rootState, null, testState() as any, null)
-        const result = warn(STEPS.modelOptions).modelOptions
-        expect(result).toEqual([warnings[0]])
+        const result = warn(Steps.modelOptions).modelOptions
+        expect(result).toEqual(modelOptionWarnings)
     })
 
 })
