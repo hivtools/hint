613ebe3833903688b7a13b3ba49315481e006dc5
Lekan Anifowoshe <anney.lake@gmail.com>
2021-10-21 14:53:34 +0100

    fix conflicts

--- src/app/static/src/app/store/root/getters.ts
+++ src/app/static/src/app/store/root/getters.ts
@@ -2,7 +2,7 @@ import {RootState} from "../../root";
 import {Getter, GetterTree} from "vuex";
 import {Error} from "../../generated"
 import {Warning} from "../../generated";
-import {Dict, StepWarnings} from "../../types";
+import {Dict} from "../../types";
 
 interface RootGetters {
     isGuest: Getter<RootState, RootState>
@@ -22,7 +22,6 @@ export const getters: RootGetters & GetterTree<RootState, RootState> = {
         return state.currentUser == "guest";
     },
 
-<<<<<<< HEAD
     warnings: (state: RootState) => (stepName: string) => {
         const filterWarnings = (warnings: Warning[], stepLocation: string) =>
             warnings.filter(warning => warning.locations.some(location => location === stepLocation))
@@ -36,8 +35,6 @@ export const getters: RootGetters & GetterTree<RootState, RootState> = {
         }
     },
 
-=======
->>>>>>> mrc-2567
     errors: (state: RootState) => {
         const {
             adr,
@@ -68,18 +65,6 @@ export const getters: RootGetters & GetterTree<RootState, RootState> = {
             extractErrors(surveyAndProgram),
             state.modelRun.errors,
             state.errors.errors]);
-    },
-    warnings: (state: RootState) => (stepName: string): StepWarnings => {
-        const filterWarnings = (warnings: Warning[], stepLocation: string) =>
-            (warnings || []).filter(warning => warning.locations.some(location => location === stepLocation))
-
-        const location = warningStepLocationMapping[stepName]
-
-        return {
-            modelOptions: filterWarnings(state.modelOptions.warnings, location),
-            modelRun: filterWarnings(state.modelRun.warnings, location),
-            modelCalibrate: filterWarnings(state.modelCalibrate.warnings, location)
-        }
     }
 }
 
@@ -99,9 +84,5 @@ const extractErrorsRecursively = (state: any, errors: Error[]) => {
         const errorKeys = keys.filter(key => /error$/i.test(key));
         errors.push(...errorKeys.map(key => state[key]).filter(err => !!err && !!err.error));
         keys.forEach(key => extractErrorsRecursively(state[key], errors));
-<<<<<<< HEAD
-
-=======
->>>>>>> mrc-2567
     }
 };
