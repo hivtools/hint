7ffbab81ff311fb4da9d47e343c7df0de25dc655
Alex <alex.hill@gmail.com>
2021-11-11 11:39:42 +0000

    placeholder loading spinner

--- src/app/static/src/app/dataExploration.ts
+++ src/app/static/src/app/dataExploration.ts
@@ -3,6 +3,7 @@ import Vuex, {mapActions, mapState} from "vuex";
 import registerTranslations from "./store/translations/registerTranslations";
 import {DataExplorationState, storeOptions} from "./store/dataExploration/dataExploration";
 import Errors from "./components/Errors.vue";
+import LoadingSpinner from "./components/LoadingSpinner.vue";
 import {Language} from "./store/translations/locales";
 
 Vue.use(Vuex);
@@ -14,7 +15,8 @@ export const dataExplorationApp = new Vue({
     el: "#app",
     store,
     components: {
-        Errors
+        Errors,
+        LoadingSpinner
     },
     computed: mapState<DataExplorationState>({
         language: (state: DataExplorationState) => state.language
