aa4f5b0a42bbe51e1a2accb00b0207e9fa6e3b2f
Nicolas Dolan <nicolas.dolan@ymail.com>
2021-11-12 16:19:11 +0000

    updated integration test

--- src/app/static/src/tests/integration/errors.itest.ts
+++ src/app/static/src/tests/integration/errors.itest.ts
@@ -1,8 +1,78 @@
+// // import {actions} from "../../app/store/root/actions";
+// // import {ErrorReportManualDetails} from "../../app/store/root/actions";
+// import {initialProjectsState} from "../../app/store/projects/projects";
+// import {RootState} from "../../app/root";
+// import {Language} from "../../app/store/translations/locales";
+// import {initialHintrVersionState} from "../../app/store/hintrVersion/hintrVersion";
+// import {actions} from "../../app/store/errors/actions";
+// import {ErrorReportManualDetails} from "../../app/store/errors/actions";
+// import {ErrorsState} from "../../app/store/errors/errors";
+
+// describe(`errors actions`, () => {
+
+//     it("can post error report", async () => {
+//         const commit = jest.fn();
+
+//         const dispatch = jest.fn();
+
+//         // const state = {
+//         //     errorReportError: null,
+//         // } as ErrorsState;
+
+//         const state = {
+//             language: Language.en,
+//             baseline: {
+//                 country: "Malawi"
+//             },
+//             modelRun: {
+//                 modelRunId: "1234"
+//             },
+//             projects: initialProjectsState(),
+//             hintrVersion: initialHintrVersionState()
+//         } as RootState;
+
+//         state.projects.currentProject = {name: "p1", id: 1, versions: []}
+//         // } as RootState;
+
+//         const getters = {
+//             errors: []
+//         }
+
+//         const payload: ErrorReportManualDetails = {
+//             email: "test@example.com",
+//             stepsToReproduce: "steps to reproduce",
+//             section: "login",
+//             description: "test"
+//         }
+
+//         await actions.generateErrorReport({commit, rootState: state, getters, dispatch} as any, payload);
+//         expect(commit.mock.calls[0][0]).toStrictEqual(
+//             {
+//                 payload: {
+//                     description: "OK",
+//                     statusCode: 200
+//                 },
+//                 type: "ErrorReportSuccess"
+//             });
+
+//         expect(dispatch.mock.calls.length).toBe(1)
+//         expect(dispatch.mock.calls[0][0]).toEqual("projects/cloneProject")
+//         expect(dispatch.mock.calls[0][1]).toEqual(
+//             {
+//                 "emails": ["naomi-support@imperial.ac.uk"],
+//                 "projectId": 1
+//             })
+//     });
+// })
+
 import {actions} from "../../app/store/errors/actions";
 import {ErrorReportManualDetails} from "../../app/store/errors/actions";
-import {ErrorsState} from "../../app/store/errors/errors";
+import {initialProjectsState} from "../../app/store/projects/projects";
+import {RootState} from "../../app/root";
+import {Language} from "../../app/store/translations/locales";
+import {initialHintrVersionState} from "../../app/store/hintrVersion/hintrVersion";
 
-describe(`errors actions`, () => {
+describe(`root actions`, () => {
 
     it("can post error report", async () => {
         const commit = jest.fn();
@@ -10,8 +80,18 @@ describe(`errors actions`, () => {
         const dispatch = jest.fn();
 
         const state = {
-            errorReportError: null,
-        } as ErrorsState;
+            language: Language.en,
+            baseline: {
+                country: "Malawi"
+            },
+            modelRun: {
+                modelRunId: "1234"
+            },
+            projects: initialProjectsState(),
+            hintrVersion: initialHintrVersionState()
+        } as RootState;
+
+        state.projects.currentProject = {name: "p1", id: 1, versions: []}
 
         const getters = {
             errors: []
@@ -24,7 +104,7 @@ describe(`errors actions`, () => {
             description: "test"
         }
 
-        await actions.generateErrorReport({commit, errorsState: state, getters, dispatch} as any, payload);
+        await actions.generateErrorReport({commit, rootState: state, getters, dispatch} as any, payload);
         expect(commit.mock.calls[0][0]).toStrictEqual(
             {
                 payload: {
