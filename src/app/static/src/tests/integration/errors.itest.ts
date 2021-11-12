b2ba65f407f6e7076e915931766714b3247cad30
Nicolas Dolan <nicolas.dolan@ymail.com>
2021-11-12 11:03:59 +0000

    adjusted tests

--- src/app/static/src/tests/integration/errors.itest.ts
+++ src/app/static/src/tests/integration/errors.itest.ts
@@ -1,11 +1,8 @@
-import {actions} from "../../app/store/root/actions";
-import {ErrorReportManualDetails} from "../../app/store/root/actions";
-import {initialProjectsState} from "../../app/store/projects/projects";
-import {RootState} from "../../app/root";
-import {Language} from "../../app/store/translations/locales";
-import {initialHintrVersionState} from "../../app/store/hintrVersion/hintrVersion";
+import {actions} from "../../app/store/errors/actions";
+import {ErrorReportManualDetails} from "../../app/store/errors/actions";
+import {ErrorsState} from "../../app/store/errors/errors";
 
-describe(`root actions`, () => {
+describe(`errors actions`, () => {
 
     it("can post error report", async () => {
         const commit = jest.fn();
@@ -13,19 +10,8 @@ describe(`root actions`, () => {
         const dispatch = jest.fn();
 
         const state = {
-            language: Language.en,
             errorReportError: null,
-            baseline: {
-                country: "Malawi"
-            },
-            modelRun: {
-                modelRunId: "1234"
-            },
-            projects: initialProjectsState(),
-            hintrVersion: initialHintrVersionState()
-        } as RootState;
-
-        state.projects.currentProject = {name: "p1", id: 1, versions: []}
+        } as ErrorsState;
 
         const getters = {
             errors: []
@@ -38,7 +24,7 @@ describe(`root actions`, () => {
             description: "test"
         }
 
-        await actions.generateErrorReport({commit, rootState: state, getters, dispatch} as any, payload);
+        await actions.generateErrorReport({commit, errorsState: state, getters, dispatch} as any, payload);
         expect(commit.mock.calls[0][0]).toStrictEqual(
             {
                 payload: {
