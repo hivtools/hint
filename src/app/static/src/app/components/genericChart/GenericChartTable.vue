635decc76849e3d6282f2e4eb887039ed71a246b
Emma Russell <EmmaRussellKilburn@gmail.com>
2021-12-08 16:24:19 +0000

    Buid dictionary of labels for label columns only

--- src/app/static/src/app/components/genericChart/GenericChartTable.vue
+++ src/app/static/src/app/components/genericChart/GenericChartTable.vue
@@ -1,6 +1,4 @@
 <template>
-    <div>
-    <div>{{hierarchyColumnPaths}}</div>
     <table-view :fields="generatedFields" :filtered-data="labelledData">
         <template v-for="column in columnsWithHierarchy" v-slot:[`cell(${column.data.columnId})`]="data">
             <div :key="column.data.columnId">
@@ -9,18 +7,13 @@
             </div>
         </template>
     </table-view>
-    </div>
 </template>
 
 <script lang="ts">
     import Vue from "vue";
     import {FilterOption, NestedFilterOption} from "../../generated";
-    import {
-        Dict, GenericChartColumn, GenericChartTableColumnConfig,
-        GenericChartTableConfig
-    } from "../../types";
+    import {Dict, GenericChartColumn, GenericChartTableColumnConfig, GenericChartTableConfig} from "../../types";
     import TableView, {Field} from "../plots/table/Table.vue";
-    import {findPath} from "../plots/utils";
 
     interface Props {
         filteredData: any[],
@@ -77,12 +70,16 @@
                 return this.tableConfig.columns.filter(column => column.data.hierarchyColumn);
             },
             hierarchyColumnPaths() {
-                console.log("Started hierarchyColumnsToPaths: " + new Date().toISOString())
+               // Generate a dictionary of dictionaries allowing hierarchy label lookup for all  hierarchy columns (i.e. area)
+               // to save the hierarchy string for each option. This traverses the entire hierarchy once only, which is
+               // quicker than calling findPath on each row value.
                const addPathsToDict = (option: NestedFilterOption, parentPath: string,  dict: Dict<string>) => {
+                   //Add the path for this item...
                    dict[option.id] = parentPath;
+                   //...then adds paths for any children
                    const path = parentPath + (parentPath.length ? "/" : "") + option.label;
                    if (option.children) {
-                       option.children.forEach((child: any) => addPathsToDict(child as any, path, dict));
+                       option.children.forEach((child) => addPathsToDict(child as NestedFilterOption, path, dict));
                    }
                };
 
@@ -94,52 +91,55 @@
                    addPathsToDict(column.values as any, "", pathsDict);
                    result[columnId] = pathsDict;
                });
-                console.log("Finished hierarchyColumnsToPaths: " + new Date().toISOString())
                return result;
             },
             labelledData() {
-                console.log("Starting labelledData: " + new Date().toISOString())
                 // Build a dictionary of the columns defined in the fetched dataset, which include all values and
                 // associated labels
                 const columnsDict = this.columns.reduce((dict, column) => ({...dict, [column.id]: column}), {} as Dict<GenericChartColumn>);
 
-                //This above gives a dict of column ids to metadata columns, with all metadata
-                // We actually want to key filter option labels by id
-                // Map each column's filter options to a dictionary of id to label
-                const columnValueLabels = Object.keys(columnsDict).reduce((dict, key) => {
-                    const filterOptions = columnsDict[key].values;
-                    const labelDict = filterOptions.reduce ? filterOptions.reduce((dict, o) => ({...dict, [o.id]: o.label}), {} as Dict<string>) : {};
-                    return {
-                        ...dict,
-                        [key]: labelDict
+                // Convert each label column's filter options to a dictionary of id to labels so we do not need to find values'
+                // labels in the options array per row
+                const configLabelColumns = this.tableConfig.columns.filter(column => column.data.labelColumn);
+                const columnValueLabels = configLabelColumns.reduce((dict, columnConfig) => {
+                    const key = columnConfig.data.labelColumn!;
+                    // If key is not already in dictionary, and options are an array (not the case for hierarchies)
+                    if (!dict[key] && columnsDict[key].values.reduce) {
+                        const filterOptions = columnsDict[key].values;
+                        const labelDict = filterOptions.reduce((dict, option) => ({
+                            ...dict,
+                            [option.id]: option.label
+                        }), {} as Dict<string>);
+
+                        return {
+                            ...dict,
+                            [key]: labelDict
+                        }
+                    } else {
+                        return dict;
                     }
                 }, {} as Dict<Dict<string>>);
-                console.log("Finished making value labels dict: " + new Date().toISOString())
 
-                const configLabelColumns = this.tableConfig.columns.filter(column => column.data.labelColumn);
                 const result = this.filteredData.map(row => {
                     const friendlyRow = {...row};
                     // Check the table configuration for each configured column, and replace data values
                     // with friendly values (labels) taken from columnValueLabels where configured to do so - i.e.
                     // when there is a 'labelColumn' value
                     configLabelColumns.forEach(columnConfig => {
-                        //const column = columnsDict[columnConfig.data.labelColumn!];
-                        //const friendlyValue = column.values.find(value => value.id == row[columnConfig.data.columnId])?.label;
-                        const rowValue = row[columnConfig.data.columnId];
-                        const friendlyValue = columnValueLabels[columnConfig.data.labelColumn!][rowValue];
-                        friendlyRow[columnConfig.data.columnId] = friendlyValue;
+                        const columnId = columnConfig.data.columnId;
+                        const rowValue = row[columnId];
+                        friendlyRow[columnId] = columnValueLabels[columnConfig.data.labelColumn!][rowValue];
                     });
+
                     // Include additional fields for any columns configured to include hierarchy values - use the
                     // configured column's data column id, suffixed with '_hierarchy' as the field id
                     this.columnsWithHierarchy.forEach((columnConfig) => {
                         const hierarchyColumn = columnsDict[columnConfig.data.hierarchyColumn!];
                         const value = row[hierarchyColumn.column_id];
-                        const hierarchy = findPath(value, hierarchyColumn.values);
-                        friendlyRow[`${columnConfig.data.columnId}_hierarchy`] = hierarchy;
+                        friendlyRow[`${columnConfig.data.columnId}_hierarchy`] = this.hierarchyColumnPaths[hierarchyColumn.id][value];
                     });
                     return friendlyRow;
                 });
-                console.log("Finished labelledData: " + new Date().toISOString())
                 return result;
             }
         },
