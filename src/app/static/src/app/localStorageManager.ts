4854f5912f73a5f7e897199487f29811c0186e8e
Emma Russell <EmmaRussellKilburn@gmail.com>
2021-12-13 14:30:21 +0000

    Split up saving of RootState and DataExplorationState to localStorage

--- src/app/static/src/app/localStorageManager.ts
+++ src/app/static/src/app/localStorageManager.ts
@@ -1,47 +1,70 @@
 import {RootState} from "./root";
 import {currentHintVersion} from "./hintVersion";
+import {DataExplorationState} from "./store/dataExploration/dataExploration";
+import {router} from "./router";
 
-const appStateKey = `hintAppState_v${currentHintVersion}`;
+const getAppStateKey = (dataExplorationMode: boolean) => {
+    const appType = dataExplorationMode ? "hintAppState_explore" : "hintAppState";
+    return `${appType}_v${currentHintVersion}`;
+};
 
-export const serialiseState = (rootState: RootState): Partial<RootState> => {
-    return {
-        version: rootState.version,
-        baseline: {
-            selectedDataset: rootState.baseline.selectedDataset,
-            selectedRelease: rootState.baseline.selectedRelease
-        } as any,
-        modelRun: {
-            ...rootState.modelRun,
-            statusPollId: -1,
-            result: null,
-            errors: []
-        },
-        modelOptions: rootState.modelOptions,
-        modelOutput: rootState.modelOutput,
-        modelCalibrate: {
-            ...rootState.modelCalibrate,
-            result: null
-        },
-        stepper: rootState.stepper,
-        metadata: {...rootState.metadata, plottingMetadataError:null},
-        plottingSelections: rootState.plottingSelections,
-        surveyAndProgram: {selectedDataType: rootState.surveyAndProgram.selectedDataType} as any,
-        projects: rootState.projects,
-        hintrVersion: rootState.hintrVersion,
-        language: rootState.language
-    };
+export const serialiseState = (state: DataExplorationState): Partial<RootState> => {
+    if (state.dataExplorationMode) {
+        return {
+            version: state.version,
+            baseline: {
+                selectedDataset: state.baseline.selectedDataset,
+                selectedRelease: state.baseline.selectedRelease
+            } as any,
+            metadata: {...state.metadata, plottingMetadataError:null},
+            plottingSelections: state.plottingSelections,
+            surveyAndProgram: {selectedDataType: state.surveyAndProgram.selectedDataType} as any,
+            hintrVersion: state.hintrVersion,
+            language: state.language
+        }
+
+    } else {
+        const rootState = state as RootState;
+        return {
+            version: state.version,
+            baseline: {
+                selectedDataset: state.baseline.selectedDataset,
+                selectedRelease: state.baseline.selectedRelease
+            } as any,
+            metadata: {...state.metadata, plottingMetadataError:null},
+            plottingSelections: state.plottingSelections,
+            surveyAndProgram: {selectedDataType: state.surveyAndProgram.selectedDataType} as any,
+            modelRun: {
+                ...rootState.modelRun,
+                statusPollId: -1,
+                result: null,
+                errors: []
+            },
+            modelOptions: rootState.modelOptions,
+            modelOutput: rootState.modelOutput,
+            modelCalibrate: {
+                ...rootState.modelCalibrate,
+                result: null
+            },
+            stepper: rootState.stepper,
+            projects: rootState.projects,
+            hintrVersion: state.hintrVersion,
+            language: state.language
+        };
+    }
 };
 
 declare const currentUser: string;
 
 export class LocalStorageManager {
 
-    saveState = (state: RootState) => {
+    saveState = (state: DataExplorationState) => {
         const partialState = serialiseState(state);
         this.savePartialState(partialState);
     };
 
     savePartialState = (partialState: Partial<RootState>) => {
+        const appStateKey = getAppStateKey(false);
         window.localStorage.setItem(appStateKey, JSON.stringify(partialState));
     };
 
@@ -51,6 +74,9 @@ export class LocalStorageManager {
             localStorage.setItem("user", currentUser);
         }
 
+        console.log("route:"  +router.currentRoute.name);
+
+        const appStateKey = getAppStateKey(false);
         const item = window.localStorage.getItem(appStateKey);
         if (item) {
             return JSON.parse(item) as Partial<RootState>;
