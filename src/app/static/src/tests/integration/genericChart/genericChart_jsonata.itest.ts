9254ce24816382057374f8de276e6e48f1fc04e7
Emma Russell <EmmaRussellKilburn@gmail.com>
2022-01-10 17:02:28 +0000

    Add test

--- src/app/static/src/tests/integration/genericChart/genericChart_jsonata.itest.ts
+++ src/app/static/src/tests/integration/genericChart/genericChart_jsonata.itest.ts
@@ -78,14 +78,31 @@ const testChartData = {
     "yAxisFormat": ".0f"
 };
 
+let inputTimeSeriesJsonata: any;
 let inputTimeSeriesJsonataResult: any;
+
+const expectedXAxis = {
+    "zeroline": false,
+    "tickvals": ["2011 Q4", "2012 Q4"],
+    "tickfont": {"color": "grey"}
+};
+
+const expectYAxis = (domainStart: number, domainEnd: number, actual: any) => {
+    expect(Object.keys(actual)).toStrictEqual(["rangemode", "zeroline", "tickformat", "tickfont", "domain"]);
+    expect(actual.rangemode).toBe("tozero");
+    expect(actual.zeroline).toBe(false);
+    expect(actual.tickformat).toBe(".0f");
+    expect(actual.tickfont).toStrictEqual({"color": "grey"});
+    expect(actual.domain[0]).toBeCloseTo(domainStart, 8);
+    expect(actual.domain[1]).toBeCloseTo(domainEnd, 8);
+};
 
 describe("inputTimeSeries jsonata", () => {
     beforeAll(async () => {
         await login();
 
         const response = await getGenericChartMetadata();
-        const inputTimeSeriesJsonata = response["input-time-series"].chartConfig[0].config;
+        inputTimeSeriesJsonata = response["input-time-series"].chartConfig[0].config;
 
         const j = jsonata(inputTimeSeriesJsonata);
         inputTimeSeriesJsonataResult = j.evaluate(testChartData);
@@ -219,25 +236,171 @@ describe("inputTimeSeries jsonata", () => {
     it("evaluates layout axes as expected", () => {
         const layout = inputTimeSeriesJsonataResult.layout;
 
-        const expectedXAxis = {
-            "zeroline": false,
-            "tickvals": ["2011 Q4", "2012 Q4"],
-            "tickfont": {"color": "grey"}
-        };
-
         expect(layout.xaxis1).toStrictEqual(expectedXAxis);
         expect(layout.xaxis2).toStrictEqual(expectedXAxis);
         expect(layout.xaxis3).toStrictEqual(expectedXAxis);
 
-        const expectYAxis = (domainStart: number, domainEnd: number, actual: any) => {
-            expect(Object.keys(actual)).toStrictEqual(["rangemode", "zeroline", "tickformat", "tickfont", "domain"]);
-            expect(actual.rangemode).toBe("tozero");
-            expect(actual.zeroline).toBe(false);
-            expect(actual.tickformat).toBe(".0f");
-            expect(actual.tickfont).toStrictEqual({"color": "grey"});
-            expect(actual.domain[0]).toBeCloseTo(domainStart, 8);
-            expect(actual.domain[1]).toBeCloseTo(domainEnd, 8);
+        expectYAxis(1, 0.7, layout.yaxis1);
+        expectYAxis(1, 0.7, layout.yaxis2);
+        expectYAxis(0.5, 0.2, layout.yaxis3);
+    });
+
+    it("evaluates separate subplots for different areas with identical names", () => {
+        const testData_DuplicateNames =  {
+            "data": [
+                {
+                    "area_id": "MWI_4_1_demo",
+                    "area_name": "Chitipa",
+                    "time_period": "2011 Q4",
+                    "value": 2116
+                },
+                {
+                    "area_id": "MWI_4_1_demo",
+                    "area_name": "Chitipa",
+                    "time_period": "2012 Q4",
+                    "value": 2663
+                },
+                {
+                    "area_id": "MWI_4_2_demo",
+                    "area_name": "Karonga",
+                    "time_period": "2011 Q4",
+                    "value": 5673
+                },
+                {
+                    "area_id": "MWI_4_2_demo",
+                    "area_name": "Karonga",
+                    "time_period": "2012 Q4",
+                    "value": 7674
+                },
+                {
+                    "area_id": "MWI_4_3_demo",
+                    "area_name": "Chitipa",
+                    "time_period": "2011 Q4",
+                    "value": 4555
+                },
+                {
+                    "area_id": "MWI_4_3_demo",
+                    "area_name": "Chitipa",
+                    "time_period": "2012 Q4",
+                    "value": 4795
+                }
+            ],
+            "subplots": {
+            "columns": 2,
+                "distinctColumn": "area_name",
+                "heightPerRow": 100,
+                "subplotsPerPage": 99,
+                "rows": 2
+            },
+            "yAxisFormat": ".0f"
         };
+
+        const j = jsonata(inputTimeSeriesJsonata);
+        inputTimeSeriesJsonataResult = j.evaluate(testChartData);
+
+        expect(JSON.stringify(inputTimeSeriesJsonataResult.data)).toBe(JSON.stringify([
+            {
+                "name": "Chitipa",
+                "showlegend": false,
+                "x": ["2011 Q4", "2012 Q4"],
+                "y": [2116, 2663],
+                "xaxis": "x1",
+                "yaxis": "y1",
+                "type": "scatter",
+                "line": {"color": "rgb(51, 51, 51)"}
+            },
+            {
+                "name": "Chitipa",
+                "showlegend": false,
+                "x": ["2011 Q4", "2012 Q4"],
+                "y": [2116, 2663],
+                "xaxis": "x1",
+                "yaxis": "y1",
+                "type": "scatter",
+                "line": {"color": "rgb(255, 51, 51)"}
+            },
+            {
+                "name": "Karonga",
+                "showlegend": false,
+                "x": ["2011 Q4", "2012 Q4"],
+                "y": [5673, 7674],
+                "xaxis": "x2",
+                "yaxis": "y2",
+                "type": "scatter",
+                "line": {"color": "rgb(51, 51, 51)"}
+            },
+            {
+                "name": "Karonga",
+                "showlegend": false,
+                "x": ["2011 Q4", "2012 Q4"],
+                "y": [5673, 7674],
+                "xaxis": "x2",
+                "yaxis": "y2",
+                "type": "scatter",
+                "line": {"color": "rgb(255, 51, 51)"}
+            },
+            {
+                "name": "Chilipa",
+                "showlegend": false,
+                "x": ["2011 Q4", "2012 Q4"],
+                "y": [4555, 4795],
+                "xaxis": "x3",
+                "yaxis": "y3",
+                "type": "scatter",
+                "line": {"color": "rgb(51, 51, 51)"}
+            },
+            {
+                "name": "Chitipa",
+                "showlegend": false,
+                "x": [],
+                "y": [],
+                "xaxis": "x3",
+                "yaxis": "y3",
+                "type": "scatter",
+                "line": {"color": "rgb(255, 51, 51)"}
+            }
+        ]));
+
+        const layout = inputTimeSeriesJsonataResult.layout;
+        expect(JSON.stringify(layout.annotations)).toBe(JSON.stringify([
+            {
+                "text": "Chitipa (MWI_4_1_demo)",
+                "textfont": {},
+                "showarrow": false,
+                "x": 0.5,
+                "xanchor": "middle",
+                "xref": "x1 domain",
+                "y": 1.1,
+                "yanchor": "middle",
+                "yref": "y1 domain"
+            },
+            {
+                "text": "Karonga (MWI_4_2_demo)",
+                "textfont": {},
+                "showarrow": false,
+                "x": 0.5,
+                "xanchor": "middle",
+                "xref": "x2 domain",
+                "y": 1.1,
+                "yanchor": "middle",
+                "yref": "y2 domain"
+            },
+            {
+                "text": "Chitipa (MWI_4_3_demo)",
+                "textfont": {},
+                "showarrow": false,
+                "x": 0.5,
+                "xanchor": "middle",
+                "xref": "x3 domain",
+                "y": 1.1,
+                "yanchor": "middle",
+                "yref": "y3 domain"
+            }
+        ]));
+
+        expect(layout.xaxis1).toStrictEqual(expectedXAxis);
+        expect(layout.xaxis2).toStrictEqual(expectedXAxis);
+        expect(layout.xaxis3).toStrictEqual(expectedXAxis);
 
         expectYAxis(1, 0.7, layout.yaxis1);
         expectYAxis(1, 0.7, layout.yaxis2);
