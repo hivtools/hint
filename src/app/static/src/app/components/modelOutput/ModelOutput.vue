e70cd70530d279a6cd87f5ba7f6569b8fa17676b
Nicolas Dolan <nicolas.dolan@ymail.com>
2022-01-26 13:03:04 +0000

    updated flattenedXAxisFilterOptionIds

--- src/app/static/src/app/components/modelOutput/ModelOutput.vue
+++ src/app/static/src/app/components/modelOutput/ModelOutput.vue
@@ -230,17 +230,11 @@
                 const xAxisId = this.barchartSelections?.xAxisId
                 let ids: string[] = []
                 if (xAxisId && this.barchartFilters?.length){
-                    for (let index = 0; index < this.barchartFilters.length; index++) {
-                        const filter = this.barchartFilters[index]
-                        if (filter.id === xAxisId){
-                            if ((filter.options[0] as NestedFilterOption).children){
-                                ids = Object.keys(flattenOptions(filter.options)) || []
-                                break
-                            } else {
-                                ids = filter.options.map((option: FilterOption) => option.id) || []
-                                break
-                            }
-                        }
+                    const filter = this.barchartFilters.find((f: Filter) => f.id === xAxisId)
+                    if (filter?.options.length && (filter.options[0] as NestedFilterOption).children){
+                        ids = Object.keys(flattenOptions(filter.options))
+                    } else if (filter?.options) {
+                        ids = filter.options.map((option: FilterOption) => option.id)
                     }
                 }
                 return ids
