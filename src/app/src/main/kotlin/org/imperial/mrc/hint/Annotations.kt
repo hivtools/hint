b60fe316641555d2e6b7c46832c9c0e81376055d
Lekan Anifowoshe <anney.lake@gmail.com>
2022-04-28 14:36:28 +0100

    Update Annotations.kt

--- src/app/src/main/kotlin/org/imperial/mrc/hint/Annotations.kt
+++ src/app/src/main/kotlin/org/imperial/mrc/hint/Annotations.kt
@@ -1,6 +1,6 @@
 package org.imperial.mrc.hint
 
-@Retention(AnnotationRetention.BINARY)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class ExcludeFromCodeCoverage
 
 typealias NoCoverage = ExcludeFromCodeCoverage
