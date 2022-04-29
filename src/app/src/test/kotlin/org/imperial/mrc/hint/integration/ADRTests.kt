47af3132bbe5374bfab881983389b1380e36bc47
Lekan Anifowoshe <anney.lake@gmail.com>
2022-04-29 15:06:15 +0100

    fix test

--- src/app/src/test/kotlin/org/imperial/mrc/hint/integration/ADRTests.kt
+++ src/app/src/test/kotlin/org/imperial/mrc/hint/integration/ADRTests.kt
@@ -77,7 +77,7 @@ class ADRTests : SecureIntegrationTests()
         assertThat(data["pjnz"].textValue()).isEqualTo("inputs-unaids-spectrum-file")
         assertThat(data["outputZip"].textValue()).isEqualTo("inputs-unaids-naomi-output-zip")
         assertThat(data["outputSummary"].textValue()).isEqualTo("inputs-unaids-naomi-report")
-        assertThat(data["outputComparison"].textValue()).isEqualTo(null)
+        assertThat(data["outputComparison"].isNull).isTrue
     }
 
     private fun getPostEntityWithKey(): HttpEntity<LinkedMultiValueMap<String, String>>
