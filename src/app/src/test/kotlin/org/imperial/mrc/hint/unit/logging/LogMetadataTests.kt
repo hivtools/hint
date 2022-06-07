e9d6338e81bdc74d0cfd127d9a50422f00fd21c9
Lekan Anifowoshe <anney.lake@gmail.com>
2022-06-07 11:33:58 +0100

    attempt to fix linting

--- src/app/src/test/kotlin/org/imperial/mrc/hint/unit/logging/LogMetadataTests.kt
+++ src/app/src/test/kotlin/org/imperial/mrc/hint/unit/logging/LogMetadataTests.kt
@@ -6,15 +6,17 @@ import org.junit.jupiter.api.Test
 
 class LogMetadataTests
 {
+    private val request = Request(
+            "POST",
+            "/project",
+            "hint",
+            Client("Safari", "127.0.0.1", "session1")
+    )
+
     private val logMetadata = LogMetadata(
             "testUser",
             AppOrigin("hint", "backend"),
-            Request(
-                    "POST",
-                    "/project",
-                    "hint",
-                    Client("Safari", "127.0.0.1", "session1")
-            ),
+            request,
             Response("responseMessage", "responseStatus"),
             ErrorMessage("error", "errorDetails", "errorTrace"),
             "Updating project note",
@@ -25,13 +27,13 @@ class LogMetadataTests
     fun `can serialise log metadata`()
     {
         assertThat(logMetadata.username).isEqualTo("testUser")
-        assertThat(logMetadata.app).isEqualTo(AppOrigin(name = "hint", type = "backend"))
-        assertThat(logMetadata.request).isEqualTo(Request(
+        assertThat(logMetadata.app).isEqualToComparingFieldByField(AppOrigin(name = "hint", type = "backend"))
+        assertThat(logMetadata.request).isEqualToComparingFieldByField(Request(
                 method = "POST",
                 path = "/project",
                 hostname = "hint",
                 client = Client(agent = "Safari", geoIp = "127.0.0.1", sessionId = "session1")))
-        assertThat(logMetadata.response).isEqualTo(Response(message = "responseMessage", status = "responseStatus"))
+        assertThat(logMetadata.response).isEqualToComparingFieldByField(Response(message = "responseMessage", status = "responseStatus"))
         assertThat(logMetadata.action).isEqualTo("Updating project note")
         assertThat(logMetadata.tags).isEqualTo(listOf("project", "notes"))
     }
