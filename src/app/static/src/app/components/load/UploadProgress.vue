71896075246dd38a87c2f09044a1728bc9b45498
Lekan Anifowoshe <anney.lake@gmail.com>
2022-07-14 10:14:15 +0100

    style progress modal

--- src/app/static/src/app/components/load/UploadProgress.vue
+++ src/app/static/src/app/components/load/UploadProgress.vue
@@ -1,11 +1,13 @@
 <template>
     <div id="upload-progress">
         <modal :open="openModal">
-            <h4 class="pb-4">
-                <span id="upload-header" class="pr-2" v-translate="'uploadFromZip'"></span>
-                <span><loading-spinner size="xs"/></span>
-            </h4>
-            <div class="progress">
+            <div class="mt-3 d-flex align-items-end h4">
+                <div class="d-flex align-items-center height-40 mr-2">
+                    <span id="upload-header" v-translate="'uploadFromZip'"></span>
+                </div>
+                <loading-spinner size="xs"></loading-spinner>
+            </div>
+            <div class="progress mt-2">
                 <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger"
                      role="progressbar"
                      aria-valuenow="100"
@@ -13,7 +15,7 @@
                      aria-valuemax="100"
                      style="width: 100%"/>
             </div>
-            <span id="progress-message" v-translate="'uploading'"></span>
+            <span id="progress-message" class="mt-2" v-translate="'uploading'"></span>
             <template v-slot:footer>
                 <button id="cancel-upload-progress"
                         type="button"
