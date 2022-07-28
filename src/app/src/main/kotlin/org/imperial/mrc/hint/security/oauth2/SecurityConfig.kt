ec244e5d4f173dc4048601fd9db3447112adc71d
Lekan Anifowoshe <anney.lake@gmail.com>
2022-07-28 13:00:45 +0100

    export env variables

--- src/app/src/main/kotlin/org/imperial/mrc/hint/security/oauth2/SecurityConfig.kt
+++ src/app/src/main/kotlin/org/imperial/mrc/hint/security/oauth2/SecurityConfig.kt
@@ -1,25 +1,14 @@
 package org.imperial.mrc.hint.security.oauth2
 
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-import org.springframework.core.Ordered
-import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.stereotype.Component
 
-@Configuration
 @EnableWebSecurity
-@Component
 class SecurityConfiguration
 {
     @Bean
-    @Throws(Exception::class)
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         return http.authorizeRequests()
             .mvcMatchers("/oauth2").permitAll()
