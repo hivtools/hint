a4338f23053ca7bcb08d7f104dd8953e4b3cac1b
Lekan Anifowoshe <anney.lake@gmail.com>
2022-07-28 09:28:04 +0100

    initial setup

---
+++ src/app/src/main/kotlin/org/imperial/mrc/hint/controllers/OauthController.kt
@@ -0,0 +1,16 @@
+package org.imperial.mrc.hint.controllers
+
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.core.oidc.user.OidcUser
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class OauthController
+{
+    @GetMapping("/oauth2")
+    fun oauth(@AuthenticationPrincipal principal: OidcUser): ResponseEntity<String> {
+        return ResponseEntity.ok("works! `${principal.userInfo}`")
+    }
+}
