3712230d47969379bed3945046f9f3c1c60b4b6b
Lekan Anifowoshe <anney.lake@gmail.com>
2022-08-03 14:01:40 +0100

    change fromADR to camelCase

--- src/app/static/src/tests/integration/load.itest.ts
+++ src/app/static/src/tests/integration/load.itest.ts
@@ -58,7 +58,7 @@ describe("load actions", () => {
                 shape: {
                     hash: shape.hash,
                     filename: shape.filename,
-                    fromADR: false
+                    fromAdr: false
                 }
             });
             expect(dispatch.mock.calls[0][1]).toStrictEqual({
