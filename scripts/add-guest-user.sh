8b6806dee5c1c6ce9f5dbe17cb15335d9a5c2521
Lekan Anifowoshe <anney.lake@gmail.com>
2022-08-10 13:27:22 +0100

    add new pludin build to executables

--- scripts/add-guest-user.sh
+++ scripts/add-guest-user.sh
@@ -4,9 +4,9 @@ NETWORK=hint_nw
 HERE=$(realpath "$(dirname $0)")
 TEST_CONFIG=$HERE/../src/app/static/scripts/test.properties
 
-image=mrcide/hint-user-cli:mrc-3507
+image=mrcide/hint-user-cli:master
 docker pull $image
 docker run --network=hint_nw \
 		-v $TEST_CONFIG:/etc/hint/config.properties \
 		$image \
-		add-user guest
+		/userCLI/bin/userCLI add-user guest
--- scripts/add-test-user.sh
+++ scripts/add-test-user.sh
@@ -4,9 +4,9 @@ NETWORK=hint_nw
 HERE=$(realpath "$(dirname $0)")
 TEST_CONFIG=$HERE/../src/app/static/scripts/test.properties
 
-image=mrcide/hint-user-cli:mrc-3507
+image=mrcide/hint-user-cli:master
 docker pull $image
 docker run --network=hint_nw \
 		-v $TEST_CONFIG:/etc/hint/config.properties \
 		$image \
-		add-user test.user@example.com password
+		/userCLI/bin/userCLI add-user test.user@example.com password
