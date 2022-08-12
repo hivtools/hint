e41a3623983160237ed0b57623b94f36d073cfc3
Nicolas Dolan <nicolas.dolan@ymail.com>
2022-08-12 15:01:09 +0100

    attempting to fix build

--- scripts/add-test-user.sh
+++ scripts/add-test-user.sh
@@ -4,7 +4,7 @@ NETWORK=hint_nw
 HERE=$(realpath "$(dirname $0)")
 TEST_CONFIG=$HERE/../src/app/static/scripts/test.properties
 
-image=mrcide/hint-user-cli:mrc-3078
+image=mrcide/hint-user-cli:mrc-3080
 docker pull $image
 docker run --network=hint_nw \
 		-v $TEST_CONFIG:/etc/hint/config.properties \
