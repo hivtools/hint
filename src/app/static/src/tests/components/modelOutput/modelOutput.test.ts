3cda8bc85257aa3a905f9bca9258bbffe2ff79c1
Nicolas Dolan <nicolas.dolan@ymail.com>
2022-08-19 10:50:16 +0100

    added feature switch tests

--- src/app/static/src/tests/components/modelOutput/modelOutput.test.ts
+++ src/app/static/src/tests/components/modelOutput/modelOutput.test.ts
@@ -20,6 +20,7 @@ import BubblePlot from "../../../app/components/plots/bubble/BubblePlot.vue";
 import {expectTranslated} from "../../testHelpers";
 import {BarchartIndicator, Filter} from "../../../app/types";
 import AreaIndicatorsTable from "../../../app/components/plots/table/AreaIndicatorsTable.vue";
+import {switches} from "../../../app/featureSwitches";
 
 const localVue = createLocalVue();
 
@@ -194,9 +195,11 @@ describe("ModelOutput component", () => {
     });
 
     it("can change tabs", () => {
+        switches.comparisonPlot = true;
 
         const store = getStore();
         const wrapper = shallowMount(ModelOutput, {store, localVue});
+        expect(wrapper.findAll(".nav-link").length).toBe(4);
 
         expect(wrapper.find(".nav-link.active").text()).toBe("Map");
         expect(wrapper.findAll("choropleth-stub").length).toBe(1);
@@ -245,6 +248,17 @@ describe("ModelOutput component", () => {
 
         expect(wrapper.findAll("#bubble-plot-container").length).toBe(0);
         expect(wrapper.findAll("bubble-plot-stub").length).toBe(0);
+    });
+
+    it("does not render comparison tab if feature switch is off", () => {
+        switches.comparisonPlot = false;
+
+        const store = getStore();
+        const wrapper = shallowMount(ModelOutput, {store, localVue});
+        expect(wrapper.findAll(".nav-link").length).toBe(3);
+        expect(wrapper.findAll(".nav-link").at(0).text()).toBe("Map");
+        expect(wrapper.findAll(".nav-link").at(1).text()).toBe("Bar");
+        expect(wrapper.findAll(".nav-link").at(2).text()).toBe("Bubble Plot");
     });
 
     it("computes chartdata", () => {
