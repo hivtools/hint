ddc8a3be5737afafb8e604b2b62eb4d82a9c0641
Lekan Anifowoshe <anney.lake@gmail.com>
2022-08-25 10:16:01 +0100

    change to UserProfile

--- scripts/add-test-user.sh
+++ scripts/add-test-user.sh
@@ -9,4 +9,4 @@ docker pull $image
 docker run --network=hint_nw \
 		-v $TEST_CONFIG:/etc/hint/config.properties \
 		$image \
-		add-user test.user@imperial.ac.uk password
+		add-user test.user@example.com password
