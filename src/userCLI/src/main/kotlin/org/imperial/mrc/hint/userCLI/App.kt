7ee5794359ae169ad4c9aee320483d7c7f8a29b8
Lekan Anifowoshe <anney.lake@gmail.com>
2022-09-01 10:38:04 +0100

    Update src/userCLI/src/main/kotlin/org/imperial/mrc/hint/userCLI/App.kt
    Co-authored-by: EmmaLRussell <44669576+EmmaLRussell@users.noreply.github.com>

--- src/userCLI/src/main/kotlin/org/imperial/mrc/hint/userCLI/App.kt
+++ src/userCLI/src/main/kotlin/org/imperial/mrc/hint/userCLI/App.kt
@@ -40,6 +40,15 @@ fun main(args: Array<String>) {
     {
         try
         {
+   // When an exception is thrown in UserLogic with key userExists, userCLI 
+   // expects the key, which it then uses to get resource bundle. However,      
+   //  the UserException handler for UserLogic extends HintException where   
+   // additional text is appended: "HintException with key $key" so the resource
+   // would not be found in the bundle.
+   // After pac4j upgrade, this issue caused corruption of the profile if not dealt
+   // with gracefully, hence extracting final word here. See associated ticket 
+   // mrc-3549  
+
             val resources = ResourceBundle.getBundle("ErrorMessageBundle", Locale("en"))
             val message = resources.getString(msg.takeLastWhile { it.isLetter() })
             System.err.println(message)
