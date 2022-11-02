ad3acc19b4fb39bef3d3d16b47fc64e2f30bab80
Lekan Anifowoshe <anney.lake@gmail.com>
2022-11-02 13:04:42 +0000

    one more fix

--- buildkite/test-browser-e2e.sh
+++ buildkite/test-browser-e2e.sh
@@ -13,14 +13,13 @@ trap cleardocker EXIT
 $HERE/run-dependencies-for-integration-tests.sh
 
 # Wait for HINT to become responsive
-sleep 20
+sleep 10
 
 # Create an image based on the shared build env that compiles and tests the front-end ADR integration
 docker build --tag hint-test-browser-e2e \
   --build-arg GIT_ID=$GIT_ID \
   -f $HERE/test-browser-e2e.dockerfile \
   .
-
 
 # Run the created image
 docker run --rm \
