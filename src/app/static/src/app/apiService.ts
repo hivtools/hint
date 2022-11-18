0d6b7aaf8879d769483460b42b75352a1f09e538
Lekan Anifowoshe <anney.lake@gmail.com>
2022-11-18 13:53:45 +0000

    cover all edge cases in try catch

--- src/app/static/src/app/apiService.ts
+++ src/app/static/src/app/apiService.ts
@@ -189,7 +189,7 @@ export class APIService<S extends string, E extends string> implements API<S, E>
     }
 
     private _handleDownloadResponse = (response: AxiosResponse) => {
-        readStream(response.data, this._commit)
+        readStream(response, this._commit)
     }
 
     //Initiates a download. NB any withSuccess mutation will be ignored for downloads.
