afe615951f2d0dec2adb65ea8fb696c22b5f98c4
Lekan Anifowoshe <anney.lake@gmail.com>
2022-11-24 15:02:06 +0000

    fix test failure

--- src/app/src/test/kotlin/org/imperial/mrc/hint/integration/ActuatorTests.kt
+++ src/app/src/test/kotlin/org/imperial/mrc/hint/integration/ActuatorTests.kt
@@ -3,7 +3,6 @@ package org.imperial.mrc.hint.integration
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.web.client.getForEntity
-import org.springframework.boot.test.web.client.postForEntity
 
 class ActuatorTests: SecureIntegrationTests()
 {
@@ -17,21 +16,21 @@ class ActuatorTests: SecureIntegrationTests()
     @Test
     fun `can see app health metrics`()
     {
-        val responseEntity = testRestTemplate.postForEntity<String>("/actuator/health")
+        val responseEntity = testRestTemplate.getForEntity<String>("/actuator/health")
         assertSuccess(responseEntity)
     }
 
     @Test
     fun `can see app http trace metrics`()
     {
-        val responseEntity = testRestTemplate.postForEntity<String>("/actuator/httptrace")
+        val responseEntity = testRestTemplate.getForEntity<String>("/actuator/httptrace")
         assertSuccess(responseEntity)
     }
 
     @Test
     fun `can see app info metrics`()
     {
-        val responseEntity = testRestTemplate.postForEntity<String>("/actuator/info")
+        val responseEntity = testRestTemplate.getForEntity<String>("/actuator/info")
         assertSuccess(responseEntity)
     }
 }
