d375b7465df485f1fc9755ba54d30dd9c05657f1
Robert Ashton <r.ashton@imperial.ac.uk>
2023-02-01 15:51:28 +0000

    More text updates following review

--- src/app/static/src/app/store/translations/locales.ts
+++ src/app/static/src/app/store/translations/locales.ts
@@ -560,17 +560,18 @@ const en: Translations = {
     privacySecureHeader: "How we keep your data secure",
     privacySecureBody: "We have put in place appropriate security measures to prevent your personal data from being accidentally lost, used or accessed in an unauthorised way, altered or disclosed. In addition, we limit access to your personal data to the administrators of Naomi, who have a business need to know, and are subject to a duty of confidentiality.",
     privacyLengthHeader: "How long will you use my personal data for?",
-    privacyLengthList: "<li>We will only retain your personal data for as long as necessary to fulfil the purposes we collected it for, including for the purposes of satisfying any legal, accounting, or reporting requirements.</li>" +
-        "               <li>We therefore will remove your data when the site is discontinued or when you request we do so.</li>",
+    privacyLengthList: "<li>We will retain your personal data indefinitely to fulfil the purposes we collected it for, including for the purposes of satisfying any legal, accounting, or reporting requirements.</li>" +
+        "               <li>We therefore will remove your data when the site is discontinued.</li>" +
+        "               <li>We will remove your data when you request we do so.</li>",
     privacyDiscloseHeader: "Disclosing your information",
     privacyDiscloseList: "<li>We may pass on your personal information if we have a legal obligation to do so, or if we have to enforce or apply our terms of use and other agreements.</li>" +
         "                 <li>We will not share your information with any other organisation for marketing, market research, or any other commercial purposes.</li>",
     privacyRightsHeader: "Your rights",
-    privacyRightsList: "<li>You can find out what information we hold about you by submitting a <a href=\"https://www.imperial.ac.uk/admin-services/secretariat/information-governance/data-protection/subject-access-requests/\">subject access request</a> to <a href=\"mailto:naomi-support@imperial.ac.uk\">naomi-support@imperial.ac.uk</a> and ask us not to use the information we collect.</li>" +
+    privacyRightsList: "<li>You can find out what information we hold about you by submitting a subject access request to <a href=\"mailto:naomi-support@imperial.ac.uk\">naomi-support@imperial.ac.uk</a> and ask us not to use the information we collect.</li>" +
         "               <li>You also have the right to have data erased if it is no longer necessary for the purpose for which it was originally collected/processed, or if there are no overriding legitimate grounds for the processing. This is sometimes known as 'the right to be forgotten'.</li>" +
         "               <li>To find out more, please see - <a href=\"https://www.imperial.ac.uk/admin-services/secretariat/information-governance/data-protection/internal-guidance/guide-10/\">Guide 12 - Individual Rights | Administration and support services | Imperial College London</a></li>",
     privacyLinksHeader: "Links to other websites",
-    privacyLinksBody: "The naomi.unaids.org website contains links to other websites. This privacy policy only applies to naomi.unaids.org and doesn't cover other websites that we link to. These services have their own terms and conditions and privacy policies. If you do go to another website from this one, read the privacy policy on that website to find out what it does with your information.",
+    privacyLinksBody: "The naomi.unaids.org website contains links to other websites. This privacy policy applies to naomi.unaids.org and shiny90.unaids.org only and doesn't cover other websites that we link to. These services have their own terms and conditions and privacy policies. If you do go to another website from this one, read the privacy policy on that website to find out what it does with your information.",
     project: "Project",
     projectDescription: "Your work is organised into projects. Each project contains its own data and settings.",
     projectHistory: "Project history",
