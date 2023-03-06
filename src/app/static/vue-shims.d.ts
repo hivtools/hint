d356238b5b0378824526030c105362dd470499b8
Mantra <mantrakusumgar@outlook.com>
2023-03-06 14:51:49 +0000

    modify vue-shims file

--- src/app/static/vue-shims.d.ts
+++ src/app/static/vue-shims.d.ts
@@ -1,9 +1,19 @@
 // src/vue-shims.d.ts
-declare module "*.vue" {
-    import Vue from "vue";
-    export default Vue;
+import { ComponentCustomProperties } from "vue";
+import { Store } from "vuex";
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue';
+  const component: DefineComponent;
+  export default component;
 }
 
-declare module '@riophae/vue-treeselect';
+// declare module "@vue/runtime-core" {
+//   interface ComponentCustomProperties {
+//     $store: Store<State>;
+//   }
+// }
+
+declare module "vue3-treeselect";
 declare module 'chartjs-plugin-error-bars';
 declare module '@reside-ic/vue-charts';
