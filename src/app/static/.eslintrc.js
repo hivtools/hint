69acb10e053425c887c683ee4d0da5fec064cb74
Mantra <mantrakusumgar@outlook.com>
2023-03-06 14:50:40 +0000

    change eslint file

--- src/app/static/.eslintrc.js
+++ src/app/static/.eslintrc.js
@@ -1,31 +1,49 @@
 module.exports = {
     root: true,
-    parser: "vue-eslint-parser",
+    // parser: "vue-eslint-parser",
     parserOptions: {
-        "parser": "@typescript-eslint/parser"
+        ecmaVersion: 2020,
     },
-    plugins: [
-        "@typescript-eslint"
-    ],
+    // plugins: [
+    //     "@typescript-eslint"
+    // ],
     extends: [
+        // "eslint:recommended",
+        // "plugin:@typescript-eslint/recommended",
+        // "plugin:vue/essential",
+        "plugin:vue/vue3-essential",
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:vue/essential"
+        "@vue/typescript/recommended",
     ],
+    env: {
+        node: true,
+    },
+    overrides: [
+        {
+          files: [
+            "**/__tests__/*.{j,t}s?(x)",
+            "**/tests/unit/**/*.spec.{j,t}s?(x)",
+          ],
+          env: {
+            jest: true,
+          },
+        },
+      ],
     rules: {
         "no-prototype-builtins": "off",
         // this is just a rule to enforce nesting script tags in vue templates
         // unfortunately it doesn't understand typescript AST so won't enforce any other
         // code indentation rules in Vue script tags
         // https://eslint.vuejs.org/rules/script-indent.html
-        "vue/script-indent": ["error", 4, {
-            "baseIndent": 1,
-            "ignores": [
-                // nested objects, excluding top level of exported object (data, methods, computed, etc.)
-                "[value.type='ObjectExpression']:not(:matches(ExportDefaultDeclaration, [left.property.name='exports']) > * > [value.type='ObjectExpression'])",
-                // nested arrays
-                "[value.type='ArrayExpression']"
-            ]
-        }]
+        // "vue/script-indent": ["error", 4, {
+        //     "baseIndent": 1,
+        //     "ignores": [
+        //         // nested objects, excluding top level of exported object (data, methods, computed, etc.)
+        //         "[value.type='ObjectExpression']:not(:matches(ExportDefaultDeclaration, [left.property.name='exports']) > * > [value.type='ObjectExpression'])",
+        //         // nested arrays
+        //         "[value.type='ArrayExpression']"
+        //     ]
+        // }],
+        'vue/multi-word-component-names': 'off'
     }
 };
