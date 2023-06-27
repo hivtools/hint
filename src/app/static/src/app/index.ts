e78a70048157d0cbee2bac83c723d8b2e2bb802f
Mantra <mantrakusumgar@outlook.com>
2023-06-27 11:05:39 +0100

    finish off adding all modes

--- src/app/static/src/app/index.ts
+++ src/app/static/src/app/index.ts
@@ -1,22 +1,21 @@
 import {createApp} from "vue";
-import {store} from "./main"
-import {router} from "./router"
-import Hint from "./components/Hint.vue"
+import {store, storeDataExploration, storePassword} from "./main"
+import {router, routerDataExploration} from "./router"
 import translate from "./directives/translate";
 import FloatingVue from "floating-vue";
+import Hint from "./components/Hint.vue"
+import HintDataExploration from "./components/HintDataExploration.vue";
+import ForgotPassword from "./components/password/ForgotPassword.vue";
+import ResetPassword from "./components/password/ResetPassword.vue";
 import 'floating-vue/dist/style.css';
 import "../scss/style.scss";
 
 const mountEl = document.querySelector("#app");
-const app = createApp(Hint, {...(mountEl as HTMLDivElement).dataset});
+const mountElDataExploration = document.querySelector("#dataExplorationApp");
+const mountElForgotPassword = document.querySelector("#forgotPasswordApp");
+const mountElResetPassword = document.querySelector("#resetPasswordApp");
 
-// TODO dis-disable warnings after most of the warnings have been fixed
-// TODO comment this warn handler out if you dare
-app.config.warnHandler = () => null;
-
-app.use(store);
-app.use(router);
-
+// tooltip options
 const options = {
     // Set float distance so that the arrow does not overlap
     // the tooltip icon. This was causing multiple mouse
@@ -33,11 +32,60 @@ const options = {
     }
 }
 
-app.use(FloatingVue, options);
+const getApp = () => {
+    if (mountEl) {
+        const app = createApp(Hint, {...(mountEl as HTMLDivElement).dataset});
+        app.use(store);
+        app.use(router);
 
-app.directive("translate", translate(store));
-app.config.globalProperties.$store = store;
+        app.use(FloatingVue, options);
 
-app.mount('#app');
+        app.directive("translate", translate(store));
+        app.config.globalProperties.$store = store;
 
-export default app;
+        app.mount('#app');
+        // TODO dis-disable warnings after most of the warnings have been fixed
+        // TODO comment this warn handler out if you dare
+        app.config.warnHandler = () => null;
+
+        return app
+    } else if (mountElDataExploration) {
+        const app = createApp(HintDataExploration, {...(mountElDataExploration as HTMLDivElement).dataset});
+        app.use(storeDataExploration);
+        app.use(routerDataExploration);
+
+        app.use(FloatingVue, options);
+        app.directive("translate", translate(storeDataExploration));
+        
+        app.mount('#dataExplorationApp');
+        // TODO dis-disable warnings after most of the warnings have been fixed
+        // TODO comment this warn handler out if you dare
+        app.config.warnHandler = () => null;
+        
+        return app
+    } else if (mountElForgotPassword) {
+        const app = createApp(ForgotPassword, {...(mountElForgotPassword as HTMLDivElement).dataset});
+        app.use(storePassword);
+
+        app.use(FloatingVue, options);
+        app.directive("translate", translate(storePassword));
+
+        app.mount("#forgotPasswordApp");
+
+        return app
+    } else if (mountElResetPassword) {
+        const app = createApp(ResetPassword, {...(mountElResetPassword as HTMLDivElement).dataset});
+        app.use(storePassword);
+
+        app.use(FloatingVue, options);
+        app.directive("translate", translate(storePassword));
+
+        app.mount("#resetPasswordApp");
+
+        return app
+    } else {
+        return {}
+    }
+}
+
+export default getApp();
--- src/app/static/src/app/router.ts
+++ src/app/static/src/app/router.ts
@@ -3,10 +3,17 @@ import Stepper from "./components/Stepper.vue";
 import Accessibility from "./components/Accessibility.vue";
 import Privacy from "./components/Privacy.vue";
 import Projects from "./components/projects/Projects.vue";
-import {store} from "./main";
+import {store, storeDataExploration} from "./main";
+import DataExploration from "./components/dataExploration/DataExploration.vue";
 
 export const beforeEnter = (to: RouteLocationNormalized, from: RouteLocationNormalized) => {
     if (store.state.currentUser === "guest" && !sessionStorage.getItem("asGuest")) {
+        window.location.assign("/login");
+    }
+}
+
+export const beforeEnterDataExploration = (to: RouteLocationNormalized, from: RouteLocationNormalized) => {
+    if (storeDataExploration.state.currentUser === "guest" && !sessionStorage.getItem("asGuest")) {
         window.location.assign("/login");
     }
 }
@@ -25,4 +32,18 @@ const routes = [
 export const router = createRouter({
     history: createWebHashHistory(),
     routes,
+});
+
+const routesDataExploration = [
+    {
+        path: "/",
+        component: DataExploration,
+        beforeEnter: beforeEnterDataExploration
+    },
+    {path: "/accessibility", component: Accessibility}
+];
+
+export const routerDataExploration = createRouter({
+    history: createWebHashHistory("/callback/explore"),
+    routes: routesDataExploration
 });
