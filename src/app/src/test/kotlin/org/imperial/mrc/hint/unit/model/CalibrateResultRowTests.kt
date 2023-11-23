47981a8965eee1426cd087a242bdc9e6dc0cd1e7
Mantra <mantrakusumgar@outlook.com>
2023-11-23 12:42:11 +0000

    fix test

--- src/app/src/test/kotlin/org/imperial/mrc/hint/unit/model/CalibrateResultRowTests.kt
+++ src/app/src/test/kotlin/org/imperial/mrc/hint/unit/model/CalibrateResultRowTests.kt
@@ -13,7 +13,8 @@ class CalibrateResultRowTests
         "sex" to "sex_test",
         "area_id" to "area_id_test",
         "mean" to 1, "upper" to 1,
-        "mode" to 1, "lower" to 1
+        "mode" to 1, "lower" to 1,
+        "area_level" to 1
     )
     val expectedJsonString = ObjectMapper().writeValueAsString(expectedJsonMap)
     val expectedJson = ObjectMapper().readTree(expectedJsonString)
