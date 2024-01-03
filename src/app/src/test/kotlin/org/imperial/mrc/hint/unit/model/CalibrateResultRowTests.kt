229a0083f35ec316717a6c0229a6116bf5418d40
Mantra <mantrakusumgar@outlook.com>
2024-01-03 15:46:40 +0000

    cleanup calibrate result row tests

--- src/app/src/test/kotlin/org/imperial/mrc/hint/unit/model/CalibrateResultRowTests.kt
+++ src/app/src/test/kotlin/org/imperial/mrc/hint/unit/model/CalibrateResultRowTests.kt
@@ -4,10 +4,18 @@ import org.junit.jupiter.api.Test
 import org.imperial.mrc.hint.models.CalibrateResultRow
 import org.imperial.mrc.hint.models.FilterQuery
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.JsonNode
 
 class CalibrateResultRowTests
 {
-    val expectedJsonMap = mapOf(
+    fun convertToJson(input: Any): JsonNode
+    {
+        val objectMapper = ObjectMapper()
+        val raw = objectMapper.writeValueAsString(input)
+        return objectMapper.readTree(raw)
+    }
+
+    val expectedRowJsonMap = mapOf(
         "indicator" to "indicator_test",
         "calendar_quarter" to "calendar_quarter_test",
         "age_group" to "age_group_test",
@@ -17,8 +25,15 @@ class CalibrateResultRowTests
         "mode" to 1, "lower" to 1,
         "area_level" to 1
     )
-    val expectedJsonString = ObjectMapper().writeValueAsString(expectedJsonMap)
-    val expectedJson = ObjectMapper().readTree(expectedJsonString)
+    val expectedRowJson = convertToJson(expectedRowJsonMap)
+    val filterQuery = FilterQuery(
+        listOf("ind1", "ind2"),
+        listOf("cal1"),
+        listOf("age1"),
+        listOf("sex1", "sex2", "sex3"),
+        listOf("area1"),
+        listOf(1, 2, 3, 4)
+    )
 
     @Test
     fun `result row serialises with correct json keys`()
@@ -31,22 +46,29 @@ class CalibrateResultRowTests
             "area_id_test",
             1, 1, 1, 1, 1
         )
-        val jsonRow = ObjectMapper().writeValueAsString(row)
-        val json = ObjectMapper().readTree(jsonRow)
-        assert(json.equals(expectedJson))
+        val json = convertToJson(row)
+        assert(json.equals(expectedRowJson))
+    }
+
+    @Test
+    fun `filter query serialises with correct json keys`()
+    {
+        val expectedFilterJsonMap = mapOf(
+            "indicator" to listOf("ind1", "ind2"),
+            "calendar_quarter" to listOf("cal1"),
+            "age_group" to listOf("age1"),
+            "sex" to listOf("sex1", "sex2", "sex3"),
+            "area_id" to listOf("area1"),
+            "area_level" to listOf(1, 2, 3, 4)
+        )
+        val expectedJsonFilterQuery = convertToJson(expectedFilterJsonMap)
+        val filterQueryJson = convertToJson(filterQuery)
+        assert(expectedJsonFilterQuery.equals(filterQueryJson))
     }
 
     @Test
     fun `filter query iterator works as expected`()
     {
-        val filterQuery = FilterQuery(
-            listOf("ind1", "ind2"),
-            listOf("cal1"),
-            listOf("age1"),
-            listOf("sex1", "sex2", "sex3"),
-            listOf("area1"),
-            listOf(1, 2, 3, 4)
-        )
         val expectedList = listOf(
             listOf("indicator", listOf("ind1", "ind2")),
             listOf("calendar_quarter", listOf("cal1")),
