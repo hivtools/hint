7eaf7ca573d80f5979e8624c78c8094b7b052717
Mantra <mantrakusumgar@outlook.com>
2024-01-11 17:02:57 +0000

    better modeloutput action

--- src/app/static/src/app/store/modelOutput/actions.ts
+++ src/app/static/src/app/store/modelOutput/actions.ts
@@ -18,50 +18,13 @@ export const actions: ActionTree<ModelOutputState, DataExplorationState> & Model
     async updateSelectedTab(context, tab) {
         const { commit, rootState } = context;
         commit({type: ModelOutputMutation.TabSelected, payload: tab});
-        if (tab === ModelOutputTabs.Comparison) return;
+        if (tab !== ModelOutputTabs.Bar) return;
         let filterSelections: Dict<PayloadFilterOption[]> = {};
-        switch (tab) {
-            case ModelOutputTabs.Bar:
-                if (!isObjEmpty(rootState.plottingSelections.barchart.selectedFilterOptions)) {
-                    filterSelections = {
-                        indicator: [{id: rootState.plottingSelections.barchart.indicatorId}],
-                        ...rootState.plottingSelections.barchart.selectedFilterOptions
-                    };
-                }
-                break;
-            case ModelOutputTabs.Bubble:
-                if (!isObjEmpty(rootState.plottingSelections.bubble.selectedFilterOptions)) {
-                    filterSelections = {
-                        indicator: [
-                            {id: rootState.plottingSelections.bubble.colorIndicatorId},
-                            {id: rootState.plottingSelections.bubble.sizeIndicatorId}
-                        ],
-                        area_level: [{id: rootState.plottingSelections.bubble.detail}],
-                        ...rootState.plottingSelections.bubble.selectedFilterOptions
-                    };
-                }
-                break;
-            case ModelOutputTabs.Map:
-                if (!isObjEmpty(rootState.plottingSelections.outputChoropleth.selectedFilterOptions)) {
-                    filterSelections = {
-                        indicator: [{id: rootState.plottingSelections.outputChoropleth.indicatorId}],
-                        area_level: [{id: rootState.plottingSelections.outputChoropleth.detail}],
-                        ...rootState.plottingSelections.outputChoropleth.selectedFilterOptions
-                    };
-                }
-                break;
-            case ModelOutputTabs.Table:
-                if (!isObjEmpty(rootState.plottingSelections.table.selectedFilterOptions)) {
-                    filterSelections = {
-                        indicator: [{id: rootState.plottingSelections.table.indicator}],
-                        ...rootState.plottingSelections.table.selectedFilterOptions
-                    };
-                }
-                break;
-            default:
-                break;
-        }
-        if (!isObjEmpty(filterSelections)) {
+        if (!isObjEmpty(rootState.plottingSelections.barchart.selectedFilterOptions)) {
+            filterSelections = {
+                indicator: [{id: rootState.plottingSelections.barchart.indicatorId}],
+                ...rootState.plottingSelections.barchart.selectedFilterOptions
+            };
             await getData(context as any, filterSelections, tab);
         }
     },
