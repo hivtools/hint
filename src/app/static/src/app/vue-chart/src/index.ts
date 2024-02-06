70539388ff76c970a60224e29c10f980576cb248
Mantra <mantrakusumgar@outlook.com>
2024-02-06 16:16:24 +0000

    the reckoning begins

--- src/app/static/src/app/vue-chart/src/index.ts
+++
@@ -1,9 +0,0 @@
-import BarChartWithFilters from "./bar/BarChartWithFilters.vue";
-import BarChartWithErrors from "./bar/BarChartWithErrors.vue";
-import FilterSelect from "./bar/FilterSelect.vue";
-
-export {
-    BarChartWithFilters,
-    BarChartWithErrors,
-    FilterSelect
-}
