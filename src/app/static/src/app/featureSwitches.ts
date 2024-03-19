fee4bcd2c45a405b78e867af4f84dd3faa4146de
Rob Ashton <r.ashton@imperial.ac.uk>
2024-03-19 15:21:16 +0000

    Update wording to use shipp tool and turn download on by default

--- src/app/static/src/app/featureSwitches.ts
+++ src/app/static/src/app/featureSwitches.ts
@@ -2,7 +2,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const modelBugReport = !urlParams.get('modelBugReport');
 const modelCalibratePlot = !urlParams.get('modelCalibratePlot');
 const comparisonOutput = !urlParams.get('comparisonOutput');
-const agywDownload = !!urlParams.get('agywDownload');
+const agywDownload = !urlParams.get('agywDownload');
 
 export const switches = {
     modelBugReport,
