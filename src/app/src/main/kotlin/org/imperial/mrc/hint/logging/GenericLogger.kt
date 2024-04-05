14793af772b2b185a1f319064ff9b277ed678178
Rob Ashton <r.ashton@imperial.ac.uk>
2024-04-05 14:52:07 +0100

    Use relative path to read calibrate result data

--- src/app/src/main/kotlin/org/imperial/mrc/hint/logging/GenericLogger.kt
+++ src/app/src/main/kotlin/org/imperial/mrc/hint/logging/GenericLogger.kt
@@ -13,4 +13,6 @@ interface GenericLogger
     fun error(request: HttpServletRequest, response: HttpServletResponse, message: String? = null)
     fun error(request: HttpServletRequest, error: Throwable?, status: HttpStatus)
     fun error(request: HttpServletRequest, error: HintException)
+
+    fun <K, V> error(message: String, additionalData: Map<K, V>? = null)
 }
