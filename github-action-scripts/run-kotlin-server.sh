026c833ceefffda6cb8f0950ad8410866c12345a
Mantra <mantrakusumgar@outlook.com>
2024-04-17 14:51:08 +0100

    smarter way of waiting for the server

---
+++ github-action-scripts/run-kotlin-server.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -ex
+
+nohup $HERE/../src/gradlew -p $HERE/../src app:bootrun > kotlin-logs.out 2>&1 &
+
+sleep 20
+
+for i in $(seq 1 10);
+do
+    echo "Waiting for kotlin server to come online..."
+    if grep -Fq "Started AppStartKt in" kotlin-logs.out
+    then
+        echo "Kotlin server started!"
+        exit 0
+    else
+        sleep 8
+    fi
+done
+
+echo "Kotlin server did not start in time"
+exit 1
