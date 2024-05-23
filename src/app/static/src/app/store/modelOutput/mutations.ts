c31d3af81d7420b82e9c81c8b7b8e0a287e53148
Rob Ashton <r.ashton@imperial.ac.uk>
2024-05-23 17:51:43 +0100

    Add output loading mutations back in, remove unused actions

--- src/app/static/src/app/store/modelOutput/mutations.ts
+++ src/app/static/src/app/store/modelOutput/mutations.ts
@@ -1,15 +1,25 @@
 import {MutationTree} from "vuex";
 import {ModelOutputState} from "./modelOutput";
-import {PayloadWithType} from "../../types";
+import {ModelOutputTabs, PayloadWithType} from "../../types";
 import {OutputPlotName} from "../plotSelections/plotSelections";
 
 export enum ModelOutputMutation {
-    TabSelected = "TabSelected"
+    TabSelected = "TabSelected",
+    SetTabLoading = "SetTabLoading"
+}
+
+type TabLoading = {
+    tab: ModelOutputTabs,
+    loading: boolean
 }
 
 export const mutations: MutationTree<ModelOutputState> = {
 
     [ModelOutputMutation.TabSelected](state: ModelOutputState, payload: PayloadWithType<OutputPlotName>) {
         state.selectedTab = payload.payload;
+    },
+
+    [ModelOutputMutation.SetTabLoading](state: ModelOutputState, payload: PayloadWithType<TabLoading>) {
+        state.loading[payload.payload.tab] = payload.payload.loading;
     },
 };
