bb829d2678df64328bce9b5432935de55e50ac25
Rob Ashton <r.ashton@imperial.ac.uk>
2024-05-24 17:30:00 +0100

    Add plot selection mutation tests

--- src/app/static/src/tests/plotSelections/mutations.test.ts
+++ src/app/static/src/tests/plotSelections/mutations.test.ts
@@ -1,4 +1,5 @@
 import {mutations} from "../../app/store/plotSelections/mutations";
+import {mockError} from "../mocks";
 
 describe("Plot selections mutations", () => {
     it("commits plot selections as expected", () => {
@@ -14,6 +15,20 @@ describe("Plot selections mutations", () => {
         expect(testState).toStrictEqual({
             barchart: { filter: "testFilter" },
             choropleth: {}
+        });
+    });
+
+    it("can set error in state", () => {
+        const testError = mockError();
+        const testState = {
+            barchart: {},
+            choropleth: {}
+        };
+        mutations.setError(testState as any, { payload: testError });
+        expect(testState).toStrictEqual({
+            barchart: {},
+            choropleth: {},
+            error: testError
         });
     });
 });
