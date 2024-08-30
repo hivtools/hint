15d5449df64d8c44a29024362aaf8eee7d993e40
Rob Ashton <robertashton94@gmail.com>
2024-08-30 12:13:18 +0100

    Set a screenshot tolerance threshold to ignore inconsequential differences

--- src/app/static/playwright.config.ts
+++ src/app/static/playwright.config.ts
@@ -25,7 +25,15 @@ const config: PlaywrightTestConfig = {
          * Maximum time expect() should wait for the condition to be met.
          * For example in `await expect(locator).toHaveText();`
          */
-        timeout: 5000
+        timeout: 5000,
+        toHaveScreenshot: {
+            // There are sometimes differences between CI and local because of
+            // a multiple of reasons I don't understand. Set a difference we accept.
+            // We might want to switch to running these tests inside docker if this leads
+            // to false positives.
+            // See https://github.com/edumserrano/playwright-adventures/tree/main/demos%2Fdocker#description
+            maxDiffPixelRatio: 0.02
+        }
     },
     fullyParallel: true,
     retries: process.env.CI ? 1 : 0,
