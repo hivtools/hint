d4379b44e46a5a78474f15cdeffd8cf0acb0f707
Rob Ashton <robertashton94@gmail.com>
2024-09-03 12:25:57 +0100

    Add tests for choropleth utils and composables

---
+++ src/app/static/src/tests/components/plots/choropleth/utils.test.ts
@@ -0,0 +1,54 @@
+import {IndicatorValuesDict, NumericRange} from "../../../../app/types";
+import {getFeatureData} from "../../../../app/components/plots/choropleth/utils";
+import {getColour} from "../../../../app/components/plots/utils";
+import {mockChoroplethIndicatorMetadata} from "../../../mocks";
+
+describe("choropleth utils", () => {
+    it("builds feature data correctly", () => {
+        const data = [
+            {
+                area_id: "MWI",
+                indicator: "prevalence",
+                mean: 0.25,
+                upper: 0.30,
+                lower: 0.20
+            },
+            {
+                area_id: "MWI_1",
+                indicator: "prevalence",
+                mean: 0.3,
+                upper: 0.35,
+                lower: 0.25
+            }
+        ]
+        const metadata = mockChoroplethIndicatorMetadata({
+            format: "0.00%"
+        });
+        const range: NumericRange = {
+            min: 0,
+            max: 1
+        }
+        const featureData = getFeatureData(
+            data,
+            metadata,
+            range
+        )
+
+        const expectedData: IndicatorValuesDict = {
+            MWI: {
+                value: 0.25,
+                lower_value: 0.20,
+                upper_value: 0.30,
+                color: getColour(0.25, metadata, range),
+            },
+            MWI_1: {
+                value: 0.3,
+                lower_value: 0.25,
+                upper_value: 0.35,
+                color: getColour(0.3, metadata, range),
+            }
+        };
+
+        expect(featureData).toStrictEqual(expectedData);
+    });
+})
