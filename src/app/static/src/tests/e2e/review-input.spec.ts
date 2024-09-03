308e2ad00cead9b3ba7b9d69f46b9a2dd675eef6
Rob Ashton <robertashton94@gmail.com>
2024-09-03 15:23:36 +0100

    Expand test for input time series plot

--- src/app/static/src/tests/e2e/review-input.spec.ts
+++ src/app/static/src/tests/e2e/review-input.spec.ts
@@ -1,7 +1,8 @@
 import {expect, test} from "./fixtures/project-page";
 import {uploadAllTestFiles} from "./utils/upload-utils";
+import {buildWaitForUpdateCallback} from "./utils/utils";
 
-test('can view review input plots', async ({ projectPage }) => {
+test("can view time series plot", async ({ projectPage }) => {
     const page = projectPage.page;
     await projectPage.createProject();
 
@@ -10,12 +11,57 @@ test('can view review input plots', async ({ projectPage }) => {
 
     // Continue button is active
     const continueButton = page.locator("#continue").nth(0);
-    await expect(continueButton).not.toHaveClass("disabled");
+    await expect(continueButton).not.toBeDisabled();
 
     // When I click continue
     await continueButton.click();
 
     // Review inputs page matches screenshot after data fetched
-    await expect(page.locator("#review-loading")).not.toBeVisible();
-    await expect(page.locator("#review-inputs")).toHaveScreenshot("review-input-landing.png");
+    await expect(page.locator("#review-loading")).toHaveCount(0);
+    await expect(page.locator("#review-inputs")).toHaveScreenshot("time-series-landing.png");
+    const waitForPlotUpdate = await buildWaitForUpdateCallback(page.locator(".plot-container.plotly"), 100, 20);
+
+    // and we're on first page
+    await expect(page.locator("#page-number")).toHaveText("Page 1 of 3");
+    await expect(page.getByRole("button", { name: "Previous page" })).toBeDisabled();
+    await expect(page.getByRole("button", { name: "Next page" })).not.toBeDisabled();
+
+    // When I click to open next page
+    await page.getByRole("button", { name: "Next page" }).click();
+
+    // We're on page 2
+    await expect(page.locator("#page-number")).toHaveText("Page 2 of 3");
+    await expect(page.getByRole("button", { name: "Previous page" })).not.toBeDisabled();
+    await expect(page.getByRole("button", { name: "Next page" })).not.toBeDisabled();
+
+    // Plot has updated
+    await waitForPlotUpdate();
+    await expect(page.locator("#review-inputs")).toHaveScreenshot("time-series-page2.png");
+
+    // When I click to open next page
+    await page.getByRole("button", { name: "Next page" }).click();
+
+    // Paging is updated
+    await expect(page.locator("#page-number")).toHaveText("Page 3 of 3");
+    await expect(page.getByRole("button", { name: "Previous page" })).not.toBeDisabled();
+    await expect(page.getByRole("button", { name: "Next page" })).toBeDisabled();
+
+    // When I change area level to region
+    await page.getByRole("button", { name: "District + Metro" }).click();
+    await page.locator("a").filter({ hasText: "Region" }).click();
+
+    // Plot has updated
+    await waitForPlotUpdate();
+    await expect(page.locator("#review-inputs")).toHaveScreenshot("time-series-region.png");
+
+    // Paging is hidden
+    await expect(page.locator("#page-number")).toHaveCount(0);
+
+    // When I change data source
+    await page.getByRole("button", { name: "ART", exact: true }).click();
+    await page.locator("a").filter({ hasText: "ANC testing" }).click();
+
+    // Plot has updated
+    await waitForPlotUpdate();
+    await expect(page.locator("#review-inputs")).toHaveScreenshot("time-series-anc.png");
 });
---
+++ src/app/static/src/tests/e2e/utils/utils.ts
@@ -0,0 +1,23 @@
+import {Locator} from "@playwright/test";
+
+// Build a callback which you can use to wait for changes to the inner HTML of some Locator
+// Can use this to wait for an update to happen before checking a screenshot.
+export const buildWaitForUpdateCallback = async (locator: Locator, timeout: number = 50, retries: number = 10) => {
+    let previousContent: string = await locator.evaluate(el => el.innerHTML);
+
+    return async () => {
+
+        for (let i = 0; i < retries; i++) {
+            const currentContent = await locator.evaluate(el => el.innerHTML);
+
+            if (currentContent !== previousContent) {
+                previousContent = currentContent;
+                return;
+            }
+
+            await new Promise(resolve => setTimeout(resolve, timeout));
+        }
+
+        throw new Error("Content did not update within the allowed retries");
+    };
+};
