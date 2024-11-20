6c90a210e8e62e9d5faa6e866af5f7499e108567
Kris Doyon <kristopher.doyon@gmail.com>
2024-11-20 12:37:10 -0500

    Add base map checkbox component

---
+++ src/app/static/src/app/components/plots/BaseMapCheckbox.vue
@@ -0,0 +1,36 @@
+<template>
+    <l-control position="topleft">
+        <div class="checkbox-wrapper">
+            <input type='checkbox' @change="toggleBaseMap" :checked="checked" />
+            <label v-translate="'showBaseMap'"></label>
+        </div>
+    </l-control>
+</template>
+
+<script setup lang="ts">
+import { LControl } from "@vue-leaflet/vue-leaflet";
+
+defineProps({
+    checked: {
+        type: Boolean,
+        required: true
+    },
+})
+
+const emit = defineEmits(['toggle-base-map'])
+
+const toggleBaseMap = () => {
+    emit('toggle-base-map')
+}
+</script>
+
+<style scoped>
+.checkbox-wrapper {
+    display: flex;
+    gap: 6px;
+    align-items: center;
+}
+label {
+    margin-bottom: 0px
+}
+</style>
