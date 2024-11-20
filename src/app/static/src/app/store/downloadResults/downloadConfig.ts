60bd6c572dcddce1b0b2cc6c608d0f592a94d388
Mantra <mantrakusumgar@outlook.com>
2024-11-20 20:20:45 +0000

    clean up downloadConfig

--- src/app/static/src/app/store/downloadResults/downloadConfig.ts
+++ src/app/static/src/app/store/downloadResults/downloadConfig.ts
@@ -11,6 +11,7 @@ export enum DownloadType {
     AGYW = "AGYW",
 }
 
+// switches to control which buttons are shown in download page
 export const downloadSwitches = {
     [DownloadType.SPECTRUM]: true,
     [DownloadType.COARSE]: true,
@@ -19,28 +20,12 @@ export const downloadSwitches = {
     [DownloadType.AGYW]: switches.agywDownload,
 };
 
-// if the values of this object are <url>s then we do a post to
-// /download/submit/<url>/<calibrate-id>
+// we do posts to /download/submit/<url>/<calibrate-id> with the body function called
 type PostObject = { url: string, body: (store: ActionContext<DownloadResultsState, RootState>) => object };
 export const downloadTypePrepareUrls: Record<DownloadType, PostObject> = {
-    [DownloadType.SPECTRUM]: {
-        url: "spectrum",
-        body: (store) => store.rootGetters.projectState
-    },
-    [DownloadType.COARSE]: {
-        url: "coarse-output",
-        body: () => ({})
-    },
-    [DownloadType.SUMMARY]: {
-        url: "summary",
-        body: () => ({})
-    },
-    [DownloadType.COMPARISON]: {
-        url: "comparison",
-        body: () => ({})
-    },
-    [DownloadType.AGYW]: {
-        url: "agyw",
-        body: () => ({})
-    }
+    [DownloadType.SPECTRUM]: { url: "spectrum", body: (store) => store.rootGetters.projectState },
+    [DownloadType.COARSE]: { url: "coarse-output", body: () => ({}) },
+    [DownloadType.SUMMARY]: { url: "summary", body: () => ({}) },
+    [DownloadType.COMPARISON]: { url: "comparison", body: () => ({}) },
+    [DownloadType.AGYW]: { url: "agyw", body: () => ({}) }
 } as const;
