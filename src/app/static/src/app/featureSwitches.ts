a932648770e5acbe73d100070458d7315a6bd9cf
Mantra <mantrakusumgar@outlook.com>
2024-11-21 19:04:45 +0000

    add comment to featureSwitches

--- src/app/static/src/app/featureSwitches.ts
+++ src/app/static/src/app/featureSwitches.ts
@@ -1,3 +1,8 @@
+// this used to be window.location.search, however the global window object
+// only occurs in a browser environment, inside playwright tests we are in
+// the node environment. because of this inconsistency globalThis was
+// introduces, this acts as whatever the global object is in the environment
+// so in node environment this is "global" and in the browser it is "window" 
 const urlParams = new URLSearchParams(globalThis.location?.search);
 const modelBugReport = !urlParams.get('modelBugReport');
 const modelCalibratePlot = !urlParams.get('modelCalibratePlot');
